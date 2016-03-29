24e6a37ef624247acd666709ef0c2c914503eb26
Kamil Biedrzycki <kbiedrzycki@ntrc.eu>
2016-03-29 16:04:39 +0200

    feat: adding Canvas component

---
+++ src/components/Canvas/Canvas.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './Canvas.scss';
+
+export default class Canvas extends Component {
+  render() {
+    return (
+      <section className="canvas">
+        <div className="canvas__container">
+
+        </div>
+      </section>
+    );
+  }
+}

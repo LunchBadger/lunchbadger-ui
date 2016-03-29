47f80387a7640ecd794e9aa10ff3495f8beaf141
Kamil Biedrzycki <kbiedrzycki@ntrc.eu>
2016-03-29 16:04:27 +0200

    feat: adding Aside component

---
+++ src/components/Aside/Aside.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import './Aside.scss';
+
+export default class Aside extends Component {
+  render() {
+    return (
+      <aside className="aside">
+
+      </aside>
+    );
+  }
+}

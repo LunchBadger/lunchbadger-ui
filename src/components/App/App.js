559b242247b28015f66f206c67d4c31d0057abb5
Arkadiusz Putko <arkadiusz.putko@gmail.com>
2016-03-30 11:56:13 +0200

    scss changed

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,8 +2,8 @@ import React, {Component} from 'react';
 import Aside from '../Aside/Aside';
 import Canvas from '../Canvas/Canvas';
 import Header from '../Header/Header';
-import './App.scss';
 import 'font-awesome/css/font-awesome.css';
+import './App.scss';
 
 export default class App extends Component {
   render() {

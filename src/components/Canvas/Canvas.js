2f29a458aa366ceeda6745aeb30dd6fed97f1646
Kamil Biedrzycki <kbiedrzycki@ntrc.eu>
2016-03-30 10:47:40 +0200

    feat: moving Quadrant to separate component, adding Quadrant resize handle

--- src/components/Canvas/Canvas.js
+++ src/components/Canvas/Canvas.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import Quadrant from './Quadrant';
+import Quadrant from './../Quadrant/Quadrant';
 import './Canvas.scss';
 
 export default class Canvas extends Component {
@@ -7,9 +7,9 @@ export default class Canvas extends Component {
     return (
       <section className="canvas">
         <div className="canvas__container">
-          <Quadrant title="Backend"/>
-          <Quadrant title="Private"/>
-          <Quadrant title="Gateways"/>
+          <Quadrant resizable title="Backend"/>
+          <Quadrant resizable title="Private"/>
+          <Quadrant resizable title="Gateways"/>
           <Quadrant title="Public"/>
         </div>
       </section>

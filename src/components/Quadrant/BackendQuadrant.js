61cc49b07eff57d6f586de34f1810a2388c9419c
Kamil Biedrzycki <kbiedrzycki@ntrc.eu>
2016-03-30 12:30:31 +0200

    feat: creating composed component for Quadrants

---
+++ src/components/Quadrant/BackendQuadrant.js
@@ -0,0 +1,29 @@
+import React, {Component, PropTypes} from 'react';
+import Quadrant from './Quadrant';
+
+class BackendQuadrant extends Component {
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    entities: PropTypes.array
+  };
+
+  constructor(props) {
+    super(props);
+  }
+
+  renderEntities() {
+    return this.props.entities.map((entity) => {
+      return <div>{entity.name}</div>;
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        {this.renderEntities()}
+      </div>
+    );
+  }
+}
+
+export default Quadrant(BackendQuadrant);
--- src/components/Quadrant/Quadrant.js
+++ src/components/Quadrant/Quadrant.js
@@ -2,59 +2,55 @@ import React, {Component, PropTypes} from 'react';
 import QuadrantResizeHandle from './QuadrantResizeHandle';
 import './Quadrant.scss';
 
-export default class Quadrant extends Component {
-  constructor(props) {
-    super(props);
+export default (ComposedComponent) => {
+  return class Quadrant extends Component {
+    static propTypes = {
+      title: PropTypes.string.isRequired,
+      resizable: PropTypes.bool,
+      data: PropTypes.object
+    };
 
-    this.state = {
-      quadrantWidth: '25',
-      entities: []
+    constructor(props) {
+      super(props);
+
+      this.state = {
+        quadrantWidth: '25',
+        entities: []
+      };
+
+      this.onStoreChange = () => {
+        this.setState({
+          entities: this.props.data.getData()
+        })
+      }
     }
 
-    this.onStoreChange = () => {
-      this.setState({
-        entities: this.props.data.getData()
-      })
+    componentDidMount() {
+      if (this.props.data) {
+        this.props.data.addChangeListener(this.onStoreChange);
+      }
     }
-  }
 
-  componentDidMount () {
-    if (this.props.data) {
-      this.props.data.addChangeListener(this.onStoreChange);
+    componentWillUnmount() {
+      if (this.props.data) {
+        this.props.data.removeChangeListener(this.onStoreChange);
+      }
     }
-  }
 
-  componentWillUnmount () {
-    if (this.props.data) {
-      this.props.data.removeChangeListener(this.onStoreChange);
-    }
-  }
-
-  renderEntities () {
-    return this.state.entities.map((entity) => {
-      return <div>{entity.name}</div>;
-    })
-  }
-
-  render() {
-    return (
-      <div className="quadrant" style={{width: `${this.state.quadrantWidth}%`}}>
-        <div className="quadrant__title">{this.props.title}</div>
-        <div className="quadrant__body">
-          {this.renderEntities()}
+    render() {
+      return (
+        <div className="quadrant" style={{width: `${this.state.quadrantWidth}%`}}>
+          <div className="quadrant__title">{this.props.title}</div>
+          <div className="quadrant__body">
+            <ComposedComponent {...this.props} entities={this.state.entities} />
+          </div>
+          {(() => {
+            if (this.props.resizable) {
+              return <QuadrantResizeHandle />;
+            }
+          })()}
         </div>
-        {(() => {
-          if (this.props.resizable) {
-            return <QuadrantResizeHandle />;
-          }
-        })()}
-      </div>
-    );
+      );
+    }
   }
 }
-
-Quadrant.propTypes = {
-  title: PropTypes.string.isRequired,
-  resizable: PropTypes.bool,
-  data: PropTypes.object
-};

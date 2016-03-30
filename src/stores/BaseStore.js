1910cb1bcf19a5cb394a6f66c529b34564be53fa
Kamil Biedrzycki <kbiedrzycki@ntrc.eu>
2016-03-30 10:27:16 +0200

    feat: creating BaseStore to handle change events

---
+++ src/stores/BaseStore.js
@@ -0,0 +1,28 @@
+import { EventEmitter } from 'events';
+import { register } from '../dispatcher/AppDispatcher';
+
+export default class BaseStore extends EventEmitter {
+  constructor() {
+    super();
+  }
+
+  subscribe(actionSubscribe) {
+    this._dispatchToken = register(actionSubscribe());
+  }
+
+  get dispatchToken() {
+    return this._dispatchToken;
+  }
+
+  emitChange() {
+    this.emit('CHANGE');
+  }
+
+  addChangeListener(callback) {
+    this.on('CHANGE', callback);
+  }
+
+  removeChangeListener(callback) {
+    this.removeListener('CHANGE', callback);
+  }
+}

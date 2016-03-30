010a8b775a18e8060b7fe1109151c412bd11d982
Arkadiusz Putko <arkadiusz.putko@gmail.com>
2016-03-30 11:52:26 +0200

    changed class name and html element

--- src/components/tools/PrivateEndpoint.js
+++ src/components/tools/PrivateEndpoint.js
@@ -2,13 +2,13 @@ import React, {Component} from 'react';
 import './PrivateEndpoint.scss';
 import AddPrivateEndpoint from 'actions/AddPrivateEndpoint';
 
-export default class Aside extends Component {
+export default class PrivateEndpoint extends Component {
   render() {
     return (
-      <privateendpoint className="privateendpoint" onClick={() => AddPrivateEndpoint()}>
+      <div className="privateendpoint" onClick={() => AddPrivateEndpoint()}>
       	<i className="privateendpoint__icon fa fa-github"></i>
       	<span className="privateendpoint__tooltip">Private Endpoint</span>
-      </privateendpoint>
+      </div>
     );
   }
 }

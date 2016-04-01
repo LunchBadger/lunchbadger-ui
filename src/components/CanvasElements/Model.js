9fddce78e1815f61ebfe6bf504a560ddcec76621
Kamil Biedrzycki <kbiedrzycki@ntrc.eu>
2016-04-01 15:25:33 +0200

    fix: removing model title, it was moved to hoc

--- src/components/CanvasElements/Model.js
+++ src/components/CanvasElements/Model.js
@@ -11,9 +11,6 @@ class Model extends Component {
   render() {
     return (
       <div>
-        <div className="canvas-element__title">
-          {this.props.entity.name}
-        </div>
         <Port way="in" className="canvas-element__port canvas-element__port--in"/>
         <Port way="out" className="canvas-element__port canvas-element__port--out"/>
       </div>

e26fa3b7324b7339973c73869265bf3d50c524aa
Kamil Biedrzycki <kbiedrzycki@ntrc.eu>
2016-04-01 12:00:27 +0200

    feat: pipeline styles

--- src/components/CanvasElements/Subelements/Pipeline.js
+++ src/components/CanvasElements/Subelements/Pipeline.js
@@ -9,8 +9,15 @@ export default class Pipeline extends Component {
 
   render() {
     return (
-      <div className="pipeline">
-        <div className="pipeline__name">{this.props.entity.name}</div>
+      <div className="pipeline pipeline--opened">
+        <div className="pipeline__details">
+          <div className="pipeline__icon">
+            <i className="fa fa-inbox"/>
+          </div>
+          <div className="pipeline__name">
+            {this.props.entity.name}
+          </div>
+        </div>
         <Port way="in" className="canvas-element__port canvas-element__port--in"/>
         <Port way="out" className="canvas-element__port canvas-element__port--out"/>
       </div>

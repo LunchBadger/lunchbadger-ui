e38d3e60443469d4467f30c892dbaed94d3b7183
Kamil Biedrzycki <kbiedrzycki@ntrc.eu>
2016-04-27 12:14:09 +0200

    feat: creating own input component

---
+++ src/components/Generics/Form/Input.js
@@ -0,0 +1,37 @@
+import React, {Component, PropTypes} from 'react';
+import {HOC} from 'formsy-react';
+
+class Input extends Component {
+  static propTypes = {
+    getValue: PropTypes.func,
+    setValue: PropTypes.func,
+    handleKeyPress: PropTypes.func,
+    handleChange: PropTypes.func,
+    className: PropTypes.string
+  };
+
+  _handleKeyPress(event) {
+    if (typeof this.props.handleKeyPress === 'function') {
+      this.props.handleKeyPress(event);
+    }
+  }
+
+  _handleChange(event) {
+    this.props.setValue(event.target.value);
+
+    if (typeof this.props.handleChange === 'function') {
+      this.props.handleChange(event);
+    }
+  }
+
+  render() {
+    return (
+      <input className={this.props.className || ''}
+             value={this.props.getValue()}
+             onKeyPress={this._handleKeyPress.bind(this)}
+             onChange={this._handleChange.bind(this)}/>
+    );
+  }
+}
+
+export default HOC(Input);

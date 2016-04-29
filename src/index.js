01cdd8833d3e0b43afa9926e2a8a5409d737bc35
Kamil Biedrzycki <kbiedrzycki@ntrc.eu>
2016-04-29 15:14:33 +0200

    chore: start core

--- src/index.js
+++ src/index.js
@@ -1,7 +1,1 @@
-import 'core-js/fn/object/assign';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/Main';
-
-// Render the main component into the dom
-ReactDOM.render(<App />, document.getElementById('app'));
+// start core...

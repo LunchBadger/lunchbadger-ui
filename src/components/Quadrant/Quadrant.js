fa3059d59c0e8f5f80aa97f14a2f61dba7ee29bb
Kamil Biedrzycki <biedrzycki.kamil@gmail.com>
2016-05-06 12:46:01 +0200

    feat: set min width of quadrant equal to initialWidth

--- src/components/Quadrant/Quadrant.js
+++ src/components/Quadrant/Quadrant.js
@@ -89,7 +89,9 @@ export default (ComposedComponent) => {
     render() {
       const {connectDropTarget} = this.props;
       return connectDropTarget(
-        <div className="quadrant" ref={(ref) => this.quadrantDOM = ref} style={{width: this.state.quadrantWidth}}>
+        <div className="quadrant"
+             ref={(ref) => this.quadrantDOM = ref}
+             style={{width: this.state.quadrantWidth, 'min-width': `${this.props.initialPercentageWidth}%`}}>
           <div className="quadrant__title">{this.props.title}</div>
           <div className="quadrant__body">
             <ComposedComponent {...this.props} ref={(ref) => this.quadrant = ref} entities={this.state.entities}/>

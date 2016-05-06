6742b4427f9e05e12904c5128fbcb8a5fc85c742
Kamil Biedrzycki <biedrzycki.kamil@gmail.com>
2016-05-06 13:19:39 +0200

    feat: moving API actions to monetize module

---
+++ src/actions/CanvasElements/API/add.js
@@ -0,0 +1,11 @@
+import API from 'models/API';
+
+const {dispatch} = LunchBadgerCore.dispatcher.AppDispatcher;
+
+export default () => {
+  dispatch('AddAPI', {
+    API: API.create({
+      name: 'API'
+    })
+  });
+};

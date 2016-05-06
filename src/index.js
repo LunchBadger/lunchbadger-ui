96700906914243084cc3cb0e201aae949c80b301
Kamil Biedrzycki <biedrzycki.kamil@gmail.com>
2016-05-06 12:56:12 +0200

    feat: moving PublicEndpoint to manage module, exposing to global

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 // Let's register plugins inside the Core, yay!
+import PublicEndpoint from './models/PublicEndpoint';
+
+// export
+let LunchBadgerManage = {
+  models: {
+    PublicEndpoint: PublicEndpoint
+  }
+};
+
+if (!global.exports && !global.module && (!global.define || !global.define.amd)) {
+  global.LunchBadgerManage = LunchBadgerManage;
+}
+
+module.exports = LunchBadgerManage;

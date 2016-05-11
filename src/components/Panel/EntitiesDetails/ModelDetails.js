8a616d12d1496f3fe21d972370742b2ca184522c
Kamil Biedrzycki <biedrzycki.kamil@gmail.com>
2016-05-11 12:46:00 +0200

    feat: register model and datasource details panel

---
+++ src/components/Panel/EntitiesDetails/ModelDetails.js
@@ -0,0 +1,132 @@
+import React, {Component, PropTypes} from 'react';
+import updateModel from 'actions/CanvasElements/Model/update';
+import ModelPropertyDetails from './ModelPropertyDetails';
+import _ from 'lodash';
+
+const BaseDetails = LunchBadgerCore.components.BaseDetails;
+const Input = LunchBadgerCore.components.Input;
+const ModelProperty = LunchBadgerManage.models.ModelProperty;
+
+class ModelDetails extends Component {
+  static propTypes = {
+    entity: PropTypes.object.isRequired
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      properties: props.entity.properties.slice(),
+      changed: false
+    }
+  }
+
+  discardChanges() {
+    // revert properties
+    this.setState({properties: this.props.entity.properties.slice()});
+  }
+
+  update(model) {
+    let data = {
+      properties: []
+    };
+
+    model.properties && model.properties.forEach((property) => {
+      data.properties.push(ModelProperty.create(property));
+    });
+
+    updateModel(this.props.entity.id, _.merge(model, data));
+  }
+
+  onAddProperty() {
+    const {properties} = this.state;
+
+    properties.push(
+      ModelProperty.create({
+        propertyIsRequired: false,
+        propertyIsIndex: false
+      })
+    );
+
+    this.setState({
+      properties: properties
+    });
+
+    this.setState({changed: true}, () => {
+      this.props.parent.checkPristine();
+    });
+  }
+
+  onRemoveProperty(property) {
+    const {properties} = this.state;
+
+    _.remove(properties, function (prop) {
+      return prop.id === property.id;
+    });
+
+    this.setState({
+      properties: properties
+    });
+
+    if (!_.isEqual(properties, this.props.entity.properties)) {
+      this.setState({changed: true});
+    } else {
+      this.setState({changed: false});
+    }
+
+    setTimeout(() => {
+      this.props.parent.checkPristine();
+    });
+  }
+
+  renderProperties() {
+    return this.state.properties.map((property, index) => {
+      return (
+        <ModelPropertyDetails index={index}
+                              key={`property-${property.id}`}
+                              onRemove={this.onRemoveProperty.bind(this)}
+                              property={property}/>
+      );
+    });
+  }
+
+  render() {
+    const {entity} = this.props;
+
+    return (
+      <div className="details-panel__container details-panel__columns">
+        <div className="details-panel__fieldset">
+          <span className="details-panel__label">Context path</span>
+          <Input className="details-panel__input"
+                 value={entity.contextPath}
+                 name="contextPath"/>
+        </div>
+        <table className="details-panel__table">
+          <thead>
+          <tr>
+            <th>Name</th>
+            <th>Data type</th>
+            <th>Default Value</th>
+            <th>Required</th>
+            <th>Is index</th>
+            <th>
+              Notes
+              <a onClick={() => this.onAddProperty()} className="details-panel__add">
+                <i className="fa fa-plus"/>
+                Add property
+              </a>
+            </th>
+            <th></th>
+          </tr>
+          </thead>
+          <tbody>
+          {this.renderProperties()}
+          </tbody>
+        </table>
+      </div>
+    )
+  }
+}
+
+export default BaseDetails(ModelDetails);
+
--- src/plugs/ComposePlugin.js
+++ src/plugs/ComposePlugin.js
@@ -3,10 +3,14 @@ import Model from '../components/Tools/Model';
 import Backend from '../stores/Backend';
 import BackendQuadrant from '../components/Quadrant/BackendQuadrant';
 import PrivateQuadrant from '../components/Quadrant/PrivateQuadrant';
+import DataSourceDetails from '../components/Panel/EntitiesDetails/DataSourceDetails';
+import ModelDetails from '../components/Panel/EntitiesDetails/ModelDetails';
+import DataSourceFactory from '../models/DataSource';
 
 const Private = LunchBadgerManage.stores.Private;
 const composePlugin = new LunchBadgerCore.models.Plugin('ComposePlugin');
 const toolGroupComponent = LunchBadgerCore.components.ToolGroup;
+const ModelFactory = LunchBadgerManage.models.Model;
 
 const tools = [
   new LunchBadgerCore.models.ToolComponent(DataSource),
@@ -21,7 +25,13 @@ const quadrants = [
   new LunchBadgerCore.models.QuadrantComponent('Private', PrivateQuadrant, Private, 1, true)
 ];
 
+const detailsPanels = [
+  new LunchBadgerCore.models.PanelDetailsComponent(DataSourceFactory.type, DataSourceDetails),
+  new LunchBadgerCore.models.PanelDetailsComponent(ModelFactory.type, ModelDetails)
+];
+
 composePlugin.registerToolGroup(toolGroup);
 composePlugin.registerQuadrants(quadrants);
+composePlugin.registerDetailsPanels(detailsPanels);
 
 export default composePlugin;

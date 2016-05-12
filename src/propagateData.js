dc66a94897aaf3c7226ee47886489b3f151cd7cc
Kamil Biedrzycki <kbiedrzycki@ntrc.eu>
2016-05-12 14:22:42 +0200

    feat: propagate data to all the stores and initialize them

--- src/propagateData.js
+++ src/propagateData.js
@@ -3,22 +3,25 @@ console.info('Pre-fetching projects data...');
 const ProjectService = LunchBadgerCore.services.ProjectService;
 const projectData = ProjectService.getAll();
 
-const initializePublic = LunchBadgerManage.actions.Stores.Public.initialize;
-const initializePrivateWithEndpoints = LunchBadgerManage.actions.Stores.Private.initialize;
-const initializePrivateWithModels = LunchBadgerCompose.actions.Stores.Private.initialize;
-const initializeGateway = LunchBadgerManage.actions.Stores.Gateway.initialize;
-const initializeBackend = LunchBadgerCompose.actions.Stores.Backend.initialize;
-
 projectData.then((response) => {
   if (Array.isArray(response.body)) {
     // right now, just load first available project
 
     const data = response.body[0];
 
-    initializeBackend(data);
-    initializePrivateWithEndpoints(data);
-    initializePrivateWithModels(data);
-    initializeGateway(data);
-    initializePublic(data);
+    if (LunchBadgerManage) {
+      LunchBadgerManage.actions.Stores.Public.initialize(data);
+      LunchBadgerManage.actions.Stores.Private.initialize(data);
+      LunchBadgerManage.actions.Stores.Gateway.initialize(data);
+    }
+
+    if (LunchBadgerCompose) {
+      LunchBadgerCompose.actions.Stores.Private.initialize(data);
+      LunchBadgerCompose.actions.Stores.Backend.initialize(data);
+    }
+
+    if (LunchBadgerMonetize) {
+      LunchBadgerMonetize.actions.Stores.Public.initialize(data);
+    }
   }
 });

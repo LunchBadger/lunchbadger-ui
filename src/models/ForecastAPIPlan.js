33c330cb87fe750f45588dae440b642eaa2452fb
Kamil Biedrzycki <biedrzycki.kamil@gmail.com>
2016-05-19 12:48:51 +0200

    feat: forecasted plan has same params

--- src/models/ForecastAPIPlan.js
+++ src/models/ForecastAPIPlan.js
@@ -3,34 +3,10 @@ const APIPlan = LunchBadgerMonetize.models.APIPlan;
 export default class ForecastAPIPlan extends APIPlan {
   static type = 'ForecastAPIPlan';
 
-  _values = [];
-
-  constructor(id, name, icon, tiers, values) {
+  constructor(id, name, icon) {
     super(id);
 
     this.name = name;
     this.icon = icon;
-    this.tiers = tiers;
-    this.values = values;
-  }
-
-  toJSON() {
-    return {
-      id: this.id,
-      name: this.name,
-      icon: this.icon,
-      tiers: this.tiers.map((tier) => {
-        return tier.toJSON()
-      }),
-      values: this.values
-    }
-  }
-
-  set values(values) {
-    this._values = values;
-  }
-
-  get values() {
-    return this._values;
   }
 }

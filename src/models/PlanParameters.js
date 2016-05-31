232b5fe000bc029ecd73a27819125e20bcd2ef58
Kamil Biedrzycki <biedrzycki.kamil@gmail.com>
2016-05-31 09:47:34 +0200

    fix: removing trailing space

--- src/models/PlanParameters.js
+++ src/models/PlanParameters.js
@@ -2,12 +2,12 @@ const BaseModel = LunchBadgerCore.models.BaseModel;
 
 export default class PlanParameters extends BaseModel {
   static type = 'PlanParameters';
-  
+
   iterableKeys = [
-    'callsPerSubscriber', 
+    'callsPerSubscriber',
     'cashPerCall'
   ];
-  
+
   construct(callsPerSubscriber, cashPerCall) {
     this.callsPerSubscriber = callsPerSubscriber || 0;
     this.cashPerCall = cashPerCall || 0;

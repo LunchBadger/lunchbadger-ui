a22405c22c9e4529c051a265625cd07207a9bacd
Kamil Biedrzycki <biedrzycki.kamil@gmail.com>
2016-06-03 09:49:47 +0200

    feat: upgrades can be treaten as downgrades

---
+++ src/actions/APIForecast/addDowngrade.js
@@ -0,0 +1,18 @@
+import Upgrade from 'models/Upgrade';
+
+const {dispatch} = LunchBadgerCore.dispatcher.AppDispatcher;
+
+export default (apiForecast, props) => {
+  const {fromPlan, toPlan, value, date} = props;
+
+  dispatch('AddUpgrade', {
+    apiForecast,
+    upgrade: Upgrade.create({
+      fromPlanId: fromPlan.id,
+      toPlanId: toPlan.id,
+      value,
+      date,
+      downgrade: true
+    })
+  });
+};

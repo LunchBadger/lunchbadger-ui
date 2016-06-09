ed2882ab9e853e51a4fb076e1c29eb0c85d8c65c
Kamil Biedrzycki <biedrzycki.kamil@gmail.com>
2016-06-09 08:46:42 +0200

    feat: disabling HMR

--- cfg/dev.js
+++ cfg/dev.js
@@ -6,18 +6,13 @@ let baseConfig = require('./base');
 let defaultSettings = require('./defaults');
 
 // Add needed plugins here
-let BowerWebpackPlugin = require('bower-webpack-plugin');
 
 let config = Object.assign({}, baseConfig, {
   entry: path.join(__dirname, '../src/index'),
   cache: true,
-  devtool: 'eval-source-map',
+  devtool: 'eval',
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin(),
-    new BowerWebpackPlugin({
-      searchResolveModulesDirectories: false
-    })
+    new webpack.NoErrorsPlugin()
   ],
   module: defaultSettings.getDefaultModules()
 });
@@ -25,7 +20,7 @@ let config = Object.assign({}, baseConfig, {
 // Add needed loaders to the defaults here
 config.module.loaders.push({
   test: /\.(js|jsx)$/,
-  loader: 'react-hot!babel-loader',
+  loader: 'babel-loader',
   include: [].concat(
     config.additionalPaths,
     [path.join(__dirname, '/../src')]

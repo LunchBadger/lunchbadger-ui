969ef268da83a5adb384269b24407c96e092c1ce
Arkadiusz Putko <arkadiusz.putko@gmail.com>
2016-06-10 16:17:16 +0200

    scale as good as can be now

---
+++ src/components/PanelComponents/Subelements/ForecastResizeHandle.js
@@ -0,0 +1,37 @@
+import React, {Component, PropTypes} from 'react';
+import classNames from 'classnames';
+
+const Draggable = LunchBadgerCore.components.Draggable;
+
+export default class ForecastResizeHandle extends Component {
+  static propTypes = {
+    resizable: PropTypes.bool.isRequired,
+    onDrag: PropTypes.func.isRequired,
+    onDragEnd: PropTypes.func
+  };
+
+  _handleDrag(event) {
+    if (this.props.resizable) {
+      this.props.onDrag(event);
+    }
+  }
+
+  _handleDragEnd(event) {
+    if (this.props.resizable && typeof this.props.onDragEnd === 'function') {
+      this.props.onDragEnd(event);
+    }
+  }
+
+  render() {
+    const resizableClass = classNames({
+      'panel__handle': true,
+      'panel__handle--resizable': this.props.resizable
+    });
+
+    return (
+      <Draggable onDrag={this._handleDrag.bind(this)} onDragEnd={this._handleDragEnd.bind(this)}>
+        <div className={resizableClass}></div>
+      </Draggable>
+    );
+  }
+}

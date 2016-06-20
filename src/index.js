fcea88e60f2281882877f063d45a81ee80f9e4c0
Kamil Biedrzycki <kbiedrzycki@ntrc.eu>
2016-06-20 15:57:15 +0200

    feat: adding trench font

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'font-awesome/css/font-awesome.css';
 import 'jsplumb';
+import './fonts/trench100free.css';
 
 const App = LunchBadgerCore.components.App;
 

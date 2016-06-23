6e02da060ee7c56af746aaacc11afee3dd960fa5
Kamil Biedrzycki <kbiedrzycki@ntrc.eu>
2016-06-23 13:32:40 +0200

    feat: using phantom ^2, tweaking demo test to work with phantomjs

--- tests/demo.js
+++ tests/demo.js
@@ -2,9 +2,11 @@ module.exports = {
 	'Demo test': function (browser) {
 		browser
 			.url(browser.launchUrl)
+			.maximizeWindow()
 			.waitForElementVisible('.app', 5000)
 			.assert.title('LunchBadger')
 			.assert.containsText('.breadcrumbs', 'Project 01')
+			.saveScreenshot('./reports/screenshots/demo/loaded.png')
 			.end();
 	}
 };

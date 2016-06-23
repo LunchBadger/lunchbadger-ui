610ea3683cb32c835b0d0b75b9b9843d13f047d2
Kamil Tekliński <kteklinski@ntrc.eu>
2016-06-23 18:00:55 +0200

    metrics icon update

--- src/plugs/MetricsPanel.js
+++ src/plugs/MetricsPanel.js
@@ -1,7 +1,7 @@
 import MetricsPanel, {METRICS_PANEL} from '../components/Panel/MetricsPanel';
 
 const metricsPlugin = new LunchBadgerCore.models.Plugin('MetricsPanelButton');
-const metricsButton = new LunchBadgerCore.models.PanelButtonComponent('fa-bar-chart', METRICS_PANEL);
+const metricsButton = new LunchBadgerCore.models.PanelButtonComponent('icon-icon-metrics', METRICS_PANEL);
 const metricsPanel = new LunchBadgerCore.models.PanelComponent(MetricsPanel);
 
 metricsPlugin.registerPanel(metricsButton, metricsPanel, 0);

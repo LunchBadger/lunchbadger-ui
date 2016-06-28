1495d1a818450fbd4e29dabaf5a90933a35cf838
Kamil Biedrzycki <kbiedrzycki@ntrc.eu>
2016-06-28 11:28:38 +0200

    feat: moving nightwatch conf to js file

---
+++ nigthwatch.conf.js
@@ -0,0 +1,64 @@
+var phantomCapabilities = {
+	"browserName": "phantomjs",
+	"javascriptEnabled": true,
+	"acceptSslCerts": true,
+	"phantomjs.binary.path": "./node_modules/phantomjs/bin/phantomjs",
+	"phantomjs.cli.args": "--webdriver=5558 --webdriver-selenium-grid-hub=http://localhost:4444",
+	"phantomjs.page.settings.userAgent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.80 Safari/537.36"
+};
+
+var chromeCapabilities = {
+	"browserName": "chrome",
+	"javascriptEnabled": true,
+	"acceptSslCerts": true
+};
+
+var testUrl = 'http://localhost:8000';
+var distUrl = 'http://lunchbadger.ntrc.eu';
+
+module.exports = {
+	"src_folders": [
+		"tests"
+	],
+	"output_folder": "reports",
+	"custom_commands_path": "",
+	"custom_assertions_path": "",
+	"page_objects_path": "",
+	"globals_path": "",
+	"selenium": {
+		"start_process": true,
+		"server_path": "./node_modules/selenium-server-standalone-jar/jar/selenium-server-standalone-2.43.1.jar",
+		"log_path": "",
+		"host": "127.0.0.1",
+		"port": 4444,
+		"cli_args": {
+			"webdriver.chrome.driver": "./node_modules/chromedriver/bin/chromedriver"
+		}
+	},
+	"test_settings": {
+		"default": {
+			"launch_url": testUrl,
+			"selenium_port": 4444,
+			"selenium_host": "localhost",
+			"silent": true,
+			"screenshots": {
+				"enabled": true,
+				"on_failure": true,
+				"on_error": false,
+				"path": "reports/screenshots"
+			},
+			"desiredCapabilities": chromeCapabilities
+		},
+		"phantom": {
+			"desiredCapabilities": phantomCapabilities
+		},
+		"dist": {
+			"launch_url": distUrl,
+			"desiredCapabilities": chromeCapabilities
+		},
+		"dist_phantom": {
+			"launch_url": distUrl,
+			"desiredCapabilities": phantomCapabilities
+		}
+	}
+};

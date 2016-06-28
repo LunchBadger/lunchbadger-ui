be0d6b3de454bbd4fa6af5aafd3f66c9b65ec725
Kamil Biedrzycki <kbiedrzycki@ntrc.eu>
2016-06-28 14:40:06 +0200

    feat: Changing assertion type

--- tests/datasource/add.js
+++ tests/datasource/add.js
@@ -1,44 +1,31 @@
 module.exports = function (browser) {
 	// .dataSource.tool
 
+	browser.expect.element('.aside--editing').to.not.be.present;
+	browser.expect.element('.canvas__container--editing').to.not.be.present;
+
 	browser.click('.dataSource.tool');
 
-	browser.isVisible('.dataSource.tool .tool__context', function (result) {
-		this.assert.equal(result.value, true);
-	});
+	browser.expect.element('.dataSource.tool .tool__context').to.be.visible;
 
 	browser.click('.dataSource.tool .tool__context .tool__context__item');
 
-	browser
-		.pause(500)
-		.isVisible('.aside--editing', function (result) {
-			this.assert.equal(result.value, true);
-		})
-		.isVisible('.canvas__container--editing', function (result) {
-			this.assert.equal(result.value, true);
-		});
+	browser.pause(500);
 
-	browser
-		.isVisible('.canvas-element.editable.expanded.DataSource', function (result) {
-			this.assert.equal(result.value, true);
-		});
+	browser.expect.element('.aside--editing').to.be.present;
+	browser.expect.element('.canvas__container--editing').to.be.present;
+	browser.expect.element('.canvas-element.editable.expanded.DataSource').to.be.present;
 
-	browser
-		.getValue('.canvas-element.editable.DataSource .canvas-element__title .canvas-element__input', function (result) {
-			this.assert.equal(result.value, 'Memory');
-		});
+	browser.expect.element('.canvas-element.editable.DataSource .canvas-element__title .canvas-element__input').to.have.value.that.equals('Memory');
 
 	browser
 		.setValue('.canvas-element.editable.DataSource .canvas-element__title .canvas-element__input', 'Memory source');
 
-	browser
-		.click('.canvas-element.editable.DataSource .canvas-element__button');
+	browser.click('.canvas-element.editable.DataSource .canvas-element__button');
 
 	browser.pause(1000);
 
-	browser.getText('.canvas-element.collapsed.DataSource:last-child .canvas-element__title', function (result) {
-		this.assert.equal(result.value, 'Memory source');
-	});
+	browser.expect.element('.canvas-element.collapsed.DataSource:last-child .canvas-element__title').text.to.equal('Memory source');
 
 	browser.pause(2000);
 };

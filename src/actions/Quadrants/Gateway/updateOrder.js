d41c3f886556256545480c526181073808d01c78
Kamil Biedrzycki <biedrzycki.kamil@gmail.com>
2016-07-04 12:29:45 +0200

    Dispatch updateOrder action only for Gateway type

--- src/actions/Quadrants/Gateway/updateOrder.js
+++ src/actions/Quadrants/Gateway/updateOrder.js
@@ -1,9 +1,13 @@
+import Gateway from '../../../models/Gateway';
+
 const {dispatch} = LunchBadgerCore.dispatcher.AppDispatcher;
 
 export default (entity, itemOrder, hoverOrder) => {
-  dispatch('UpdateGatewayOrder', {
-    entity,
-    itemOrder,
-    hoverOrder
-  });
+  if (entity.constructor.type === Gateway.type) {
+    dispatch('UpdateGatewayOrder', {
+      entity,
+      itemOrder,
+      hoverOrder
+    });
+  }
 };

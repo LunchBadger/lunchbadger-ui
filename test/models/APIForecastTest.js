d96891fd2e75a05acb43c4d3b3624217b3951778
Kamil Biedrzycki <kbiedrzycki@ntrc.eu>
2016-07-05 14:37:04 +0200

    feat(test): add spec for APIForecast model

---
+++ test/models/APIForecastTest.js
@@ -0,0 +1,28 @@
+import APIForecast from 'models/APIForecast';
+import ForecastAPI from 'models/ForecastAPI';
+import uuid from 'uuid';
+
+const {API} = LunchBadgerMonetize.models;
+
+describe('APIForecast Model', () => {
+  it('should serialize APIForecast to JSON', () => {
+    const api = ForecastAPI.create({});
+    const forecast = APIForecast.create({
+      api: api,
+      id: uuid.v4()
+    });
+
+    const serializedForecast = forecast.toJSON();
+
+    expect(serializedForecast.id).to.equal(forecast.id);
+  });
+
+  it('should create ForecastAPI instance from API model', () => {
+    const api = API.create({});
+    const forecast = APIForecast.create({
+      api: api
+    });
+
+    expect(forecast.api.constructor.type).to.equal(ForecastAPI.type);
+  });
+});

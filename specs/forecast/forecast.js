7db9bf100473fba14127786d04d1fc8d51292fb5
Kamil Biedrzycki <kbiedrzycki@ntrc.eu>
2016-07-05 10:13:36 +0200

    Add specs for forecaster

---
+++ specs/forecast/forecast.js
@@ -0,0 +1,116 @@
+var page;
+var apiSelector = '.quadrant:nth-child(4) .canvas-element.API';
+var forecasterSelector = '.panel:nth-child(3) .panel__container .panel__body';
+var apiForecastSelector = forecasterSelector + ' .api-forecast';
+
+var moment = require('moment');
+
+module.exports = {
+	// '@disabled': true,
+	before: function (browser) {
+		page = browser.page.lunchBadger();
+
+		// first close forecaster if already exists
+		browser.element('css selector', apiForecastSelector + ' .api-forecast__header__nav li:nth-child(1) > a', function (result) {
+			if (result.status > -1) {
+				browser.click(apiForecastSelector + ' .api-forecast__header__nav li:nth-child(1) > a');
+			}
+		});
+
+		page.open();
+	},
+
+	'Forecast: create': function (browser) {
+		page.click('@forecaster');
+		browser.pause(1500);
+
+		page.dragDropElement(
+			apiSelector,
+			forecasterSelector
+		);
+
+		browser.expect.element(apiForecastSelector).to.be.present;
+
+		browser.getText(apiSelector + ' .canvas-element__name', function (result) {
+			browser.expect.element(apiForecastSelector + ' .api-forecast__header__title').text.to.equal(result.value);
+		});
+
+		browser.pause(2000);
+	},
+
+	'Forecast: expand': function (browser) {
+		browser.click(apiForecastSelector + ' .api-forecast__header__nav li:nth-child(2) > a');
+		browser.expect.element(apiForecastSelector + '.expanded').to.be.present;
+
+		browser.pause(2000);
+	},
+
+	'Forecast: collapse': function (browser) {
+		browser.click(apiForecastSelector + ' .api-forecast__header__nav li:nth-child(2) > a');
+		browser.expect.element(apiForecastSelector + '.expanded').to.not.be.present;
+
+		browser.pause(2000);
+	},
+
+	'Forecast: forecast next month': function (browser) {
+		var markWidth = 0;
+
+		browser.click(apiForecastSelector + ' .api-forecast__header__nav li:nth-child(2) > a');
+		browser.expect.element(apiForecastSelector + '.expanded').to.be.present;
+
+		browser.pause(500);
+
+		browser.expect.element(apiForecastSelector + ' .date-slider__mark.current').text.to.equal(moment().format('MMM')[0]);
+		browser.elements('css selector', apiForecastSelector + ' .barlayer .trace:nth-child(3) .points path', function (result) {
+			browser.assert.equal(result.value.length, parseInt(moment().format('M'), 10));
+		});
+
+		browser.getElementSize(apiForecastSelector + ' .date-slider__mark.current', function (result) {
+			markWidth = result.value.width;
+		});
+
+		browser
+			.pause(500)
+			.useCss()
+			.moveToElement(apiForecastSelector + ' .rc-slider-handle:first-child', 10, 10)
+			.mouseButtonDown(0)
+			.moveToElement(apiForecastSelector + ' .date-slider__mark.current + .date-slider__mark', 10, 10)
+			.mouseButtonUp(0)
+			.pause(500);
+
+		browser.elements('css selector', apiForecastSelector + ' .barlayer .trace:nth-child(3) .points path', function (result) {
+			browser.assert.equal(result.value.length, parseInt(moment().add(1, 'months').format('M'), 10));
+		});
+
+		browser.pause(2000);
+	},
+
+	'Forecast: change current month': function (browser) {
+		browser
+			.pause(500)
+			.useCss()
+			.moveToElement(apiForecastSelector + ' .barlayer .trace:nth-child(3) .points path:last-child', 10, 10)
+			.mouseButtonClick(0)
+			.pause(500);
+
+		browser.expect.element(apiForecastSelector + ' .date-slider__mark.selected').text.to.equal(moment().add(1, 'months').format('MMM')[0]);
+
+		browser.pause(2000);
+	},
+
+	'Forecast: change date range': function (browser) {
+	},
+
+	'Forecast: remove': function (browser) {
+		browser.click(apiForecastSelector + ' .api-forecast__header__nav li:nth-child(1) > a');
+		browser.pause(500);
+
+		browser.expect.element(apiForecastSelector).to.not.be.present;
+
+		browser.pause(1500);
+	},
+
+	after: function () {
+		page.close();
+	}
+};

3040b969f7deaeb393272565386ba1e64ceea933
Kamil Biedrzycki <kbiedrzycki@ntrc.eu>
2016-07-05 14:18:25 +0200

    feat(test): tests for Forecast store logic

--- test/loadtests.js
+++ test/loadtests.js
@@ -2,6 +2,10 @@
 
 require('babel-polyfill');
 require('core-js/fn/object/assign');
+require('../../lunchbadger-core/dist/core');
+require('../../lunchbadger-manage/dist/manage');
+require('../../lunchbadger-compose/dist/compose');
+require('../../lunchbadger-monetize/dist/monetize');
 
 // Add support for all files in the test directory
 const testsContext = require.context('.', true, /(Test\.js$)|(Helper\.js$)/);

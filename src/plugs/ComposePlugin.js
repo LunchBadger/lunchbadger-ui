5a1df7350e493f0f4f1faf2bea9f5d81d0c17d3e
Kamil Biedrzycki <biedrzycki.kamil@gmail.com>
2016-07-08 07:34:24 +0200

    feat: Change order of icons in palette

--- src/plugs/ComposePlugin.js
+++ src/plugs/ComposePlugin.js
@@ -19,7 +19,7 @@ const tools = [
   new LunchBadgerCore.models.ToolComponent(Model)
 ];
 const toolGroup = [
-  new LunchBadgerCore.models.ToolGroupComponent('compose', toolGroupComponent, tools, 5)
+  new LunchBadgerCore.models.ToolGroupComponent('compose', toolGroupComponent, tools, 1)
 ];
 
 const quadrants = [

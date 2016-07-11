9d5cf90de7ef06be32f4dad17f5f6bc01ac36001
Kamil Biedrzycki <biedrzycki.kamil@gmail.com>
2016-07-11 11:19:25 +0200

    feat: add default plans to api

--- src/models/API.js
+++ src/models/API.js
@@ -4,6 +4,12 @@ import APIPlan from './APIPlan';
 const PublicEndpoint = LunchBadgerManage.models.PublicEndpoint;
 const BaseModel = LunchBadgerCore.models.BaseModel;
 
+const defaultPlans = [
+  APIPlan.create({name: 'Minnows', icon: 'fa-paper-plane'}),
+  APIPlan.create({name: 'Dolphins', icon: 'fa-plane'}),
+  APIPlan.create({name: 'Whales', icon: 'fa-fighter-jet'})
+];
+
 export default class API extends BaseModel {
   static type = 'API';
 
@@ -25,6 +31,7 @@ export default class API extends BaseModel {
     super(id);
 
     this.name = name;
+    this.plans = defaultPlans.slice();
   }
 
   toJSON() {

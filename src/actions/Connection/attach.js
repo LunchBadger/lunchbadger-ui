6bbfe448bc98f7d4456dd6a60d0b332e1410fd78
Kamil Biedrzycki <biedrzycki.kamil@gmail.com>
2016-07-19 11:47:25 +0200

    feat: check for already existing connection

--- src/actions/Connection/attach.js
+++ src/actions/Connection/attach.js
@@ -2,12 +2,14 @@ import {notify} from 'react-notify-toast';
 import {dispatch} from 'dispatcher/AppDispatcher';
 
 export default (connectionInfo) => {
-  connectionInfo.connection.setType('wip');
+  if (!connectionInfo.connection.getParameter('existing')) {
+    connectionInfo.connection.setType('wip');
 
-  setTimeout(() => {
-    connectionInfo.connection.removeType('wip');
-    notify.show('Model successfully attached to data source', 'success');
-  }, 3000);
+    setTimeout(() => {
+      connectionInfo.connection.removeType('wip');
+      notify.show('Model successfully attached to data source', 'success');
+    }, 3000);
+  }
 
   dispatch('AddConnection', {
     from: connectionInfo.sourceId,
--- src/actions/Connection/reattach.js
+++ src/actions/Connection/reattach.js
@@ -2,12 +2,14 @@ import {notify} from 'react-notify-toast';
 import {dispatch} from 'dispatcher/AppDispatcher';
 
 export default (connectionInfo) => {
-  connectionInfo.connection.setType('wip');
+  if (!connectionInfo.connection.getParameter('existing')) {
+    connectionInfo.connection.setType('wip');
 
-  setTimeout(() => {
-    connectionInfo.connection.removeType('wip');
-    notify.show('Model successfully reattached to data source', 'success');
-  }, 3000);
+    setTimeout(() => {
+      connectionInfo.connection.removeType('wip');
+      notify.show('Model successfully reattached to data source', 'success');
+    }, 3000);
+  }
 
   dispatch('MoveConnection', {
     from: connectionInfo.originalSourceId,

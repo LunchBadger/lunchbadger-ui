f8980a9d624c9f3db408684e402e37f88ed8372d
Kamil Biedrzycki <biedrzycki.kamil@gmail.com>
2016-07-19 11:17:18 +0200

    feat: timeout animation to prevent elements cutting?

--- src/components/Panel/Panel.js
+++ src/components/Panel/Panel.js
@@ -60,6 +60,14 @@ export default (ComposedComponent) => {
         return;
       }
 
+      if (this.state.opened && !prevState.opened) {
+        setTimeout(() => {
+          this.canvas.setState({canvasHeight: this.containerBBox.height - parseInt(this.state.height, 10)});
+        }, 1000);
+
+        return;
+      }
+
       if (this.state.opened) {
         this.canvas.setState({canvasHeight: this.containerBBox.height - parseInt(this.state.height, 10)});
       } else {

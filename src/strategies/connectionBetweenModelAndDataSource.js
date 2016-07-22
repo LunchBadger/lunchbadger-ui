43eb3fe647ffd2d29f09a0d15dbae75c5939efc4
Kamil Biedrzycki <biedrzycki.kamil@gmail.com>
2016-07-22 10:15:55 +0200

    feat: check connection fullfilment status

--- src/strategies/connectionBetweenModelAndDataSource.js
+++ src/strategies/connectionBetweenModelAndDataSource.js
@@ -1,15 +1,29 @@
 import Backend from '../stores/Backend';
 
+const Connection = LunchBadgerCore.stores.Connection;
 const Private = LunchBadgerManage.stores.Private;
 const Strategy = LunchBadgerCore.models.Strategy;
 const attachConnection = LunchBadgerCore.actions.Connection.attachConnection;
 const reattachConnection = LunchBadgerCore.actions.Connection.reattachConnection;
 
-const checkConnection = (connectionInfo) => {
+const checkConnection = (connectionInfo, paper) => {
   const isBackend = Backend.findEntity(connectionInfo.sourceId) || Backend.findEntity(connectionInfo.targetId);
   const isPrivate = Private.findEntity(connectionInfo.targetId) || Private.findEntity(connectionInfo.sourceId);
 
-  return isBackend && isPrivate;
+  if (isBackend && isPrivate) {
+    if (Connection.search({toId: Connection.formatId(connectionInfo.targetId)}).length ||
+      Connection.search({toId: Connection.formatId(connectionInfo.sourceId)}).length) {
+      paper.detach(connectionInfo.connection, {
+        fireEvent: false
+      });
+
+      return false;
+    }
+
+    return true;
+  }
+
+  return null;
 };
 
 const handleConnectionCreate = new Strategy(checkConnection, attachConnection);

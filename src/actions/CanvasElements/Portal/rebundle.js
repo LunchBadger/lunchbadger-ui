d021f5db5b457e50dc7ae30c9b196bff83df605a
Kamil Biedrzycki <biedrzycki.kamil@gmail.com>
2016-07-27 13:21:06 +0200

    feat: simulate deploy when changing portals

--- src/actions/CanvasElements/Portal/rebundle.js
+++ src/actions/CanvasElements/Portal/rebundle.js
@@ -1,6 +1,11 @@
+import update from './update';
+
 const {dispatch} = LunchBadgerCore.dispatcher.AppDispatcher;
 
 export default (fromPortal, toPortal, api) => {
+  update(fromPortal.id, {});
+  update(toPortal.id, {});
+
   dispatch('RebundlePortal', {
     fromPortal,
     toPortal,
--- src/actions/CanvasElements/Portal/update.js
+++ src/actions/CanvasElements/Portal/update.js
@@ -1,8 +1,15 @@
 const {dispatch} = LunchBadgerCore.dispatcher.AppDispatcher;
 
 export default (id, props) => {
+  setTimeout(() => {
+    dispatch('UpdatePortal', {
+      id: id,
+      data: {ready: true}
+    });
+  }, 2000);
+
   dispatch('UpdatePortal', {
     id: id,
-    data: {...props}
+    data: {...props, ready: false}
   });
 };

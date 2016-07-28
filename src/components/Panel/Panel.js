e4472c016f1229d2e054fecf0b3f7c05df3b4f67
Kamil Biedrzycki <biedrzycki.kamil@gmail.com>
2016-07-28 10:23:08 +0200

    feat: change way how container height is calculated when panel is opening

--- src/components/Panel/Panel.js
+++ src/components/Panel/Panel.js
@@ -56,21 +56,24 @@ export default (ComposedComponent) => {
     }
 
     componentDidUpdate(prevProps, prevState) {
-
-      if (this.state.opened !== prevState.opened) {
-        clearTimeout(this.openTimeout);
-
-        if (this.state.opened) {
-          this.openTimeout = setTimeout(() => {
-            this.canvas.setState({canvasHeight: this.containerBBox.height - parseInt(this.state.height, 10)});
-          }, 1500);
-
-          return;
-        }
+      if (this.state.opened === prevState.opened && this.state.height === prevState.height) {
+        return;
       }
 
-      if (this.state.opened && this.state.height !== prevState.height) {
-        this.canvas.setState({canvasHeight: this.containerBBox.height - parseInt(this.state.height, 10)});
+      const heightToCalculation = this.state.height === '50vh' ? this.containerBBox.height / 2 : this.state.height;
+
+      clearTimeout(this.openTimeout);
+
+      if (this.state.opened && !prevState.opened) {
+        this.openTimeout = setTimeout(() => {
+          this.canvas.setState({canvasHeight: this.containerBBox.height - parseInt(heightToCalculation, 10)});
+        }, 1500);
+
+        return;
+      }
+
+      if (this.state.opened) {
+        this.canvas.setState({canvasHeight: this.containerBBox.height - parseInt(heightToCalculation, 10)});
       } else {
         this.canvas.setState({canvasHeight: null});
       }

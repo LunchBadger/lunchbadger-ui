7fdbf26295efdd21e1c12ee61540745b025e1986
Roman Lisagor <rlisagor@gmail.com>
2016-09-12 10:23:18 -0700

    Display user name and environment in UI

--- plugins/lunchbadger-core/src/components/Breadcrumbs/Breadcrumbs.js
+++ plugins/lunchbadger-core/src/components/Breadcrumbs/Breadcrumbs.js
@@ -1,11 +1,17 @@
-import React, {Component} from 'react';
+import React, {Component, PropTypes} from 'react';
 import './Breadcrumbs.scss';
 
 export default class Breadcrumbs extends Component {
+  static contextTypes = {
+    lunchbadgerConfig: PropTypes.object,
+    user: PropTypes.object
+  }
+
   render() {
     return (
       <div className="breadcrumbs">
-        <span className="breadcrumbs__element">Project 01</span>
+        <span className="breadcrumbs__element">{this.context.user.profile.preferred_username}</span>
+        <span className="breadcrumbs__element">{this.context.lunchbadgerConfig.envId}</span>
         <span className="breadcrumbs__element">Canvas</span>
       </div>
     );

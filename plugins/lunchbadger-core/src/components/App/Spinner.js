a235c95a752f5f5767c5b562b9fa479951c3ef7e
Roman Lisagor <rlisagor@gmail.com>
2016-09-13 15:13:12 -0700

    Give the spinner a bit of grace time.
    This prevents it from showing up if the request takes only a brief moment.

--- plugins/lunchbadger-core/src/components/App/Spinner.js
+++ plugins/lunchbadger-core/src/components/App/Spinner.js
@@ -3,11 +3,52 @@ import './Spinner.scss';
 
 export default class Spinner extends Component {
   static propTypes = {
-    loading: PropTypes.bool
+    loading: PropTypes.bool,
+    grace: PropTypes.number
+  }
+
+  static defaultProps = {
+    grace: 500
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      graceElapsed: false
+    };
+    this.timeout = null;
+  }
+
+  componentDidMount() {
+    this.start();
+  }
+
+  componentWillUnmount() {
+    if (this.timeout) {
+      clearTimeout(this.timeout);
+    }
+  }
+
+  start() {
+    this.timeout = setTimeout(() => {
+      this.timeout = null;
+      this.setState({graceElapsed: true});
+    }, this.props.grace);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.loading && !nextProps.loading) {
+      if (this.timeout) {
+        clearTimeout(this.timeout);
+      }
+      this.setState({graceElapsed: false});
+    } else if (!this.props.loading && nextProps.loading) {
+      this.start();
+    }
   }
 
   render() {
-    if (this.props.loading) {
+    if (this.state.graceElapsed) {
       return (
         <div id="spinner__overlay">
           <div id="spinner"></div>

417e6bdf8561c9b11d8f338ce7159251dc9e5f6c
Roman Lisagor <rlisagor@gmail.com>
2016-11-10 09:58:37 -0800

    Attach and reattach connection actions between DataSource and Model

--- plugins/lunchbadger-compose/src/actions/Connection/attach.js
+++ plugins/lunchbadger-compose/src/actions/Connection/attach.js
@@ -1,16 +1,33 @@
-import {notify} from 'react-notify-toast';
+import Backend from '../../stores/Backend';
 
 const {dispatch} = LunchBadgerCore.dispatcher.AppDispatcher;
+const Connection = LunchBadgerCore.stores.Connection;
+const Private = LunchBadgerManage.stores.Private;
+const ProjectService = LunchBadgerCore.services.ProjectService;
 
 export default (connectionInfo) => {
-  if (!connectionInfo.connection.getParameter('existing')) {
-    connectionInfo.connection.setType('wip');
+  connectionInfo.connection.setType('wip');
 
-    setTimeout(() => {
-      connectionInfo.connection.removeType('wip');
-      notify.show('Model successfully attached to data source', 'success');
-    }, 3000);
-  }
+  let {sourceId, targetId} = connectionInfo;
+  let dataSource = Backend.findEntity(Connection.formatId(sourceId));
+  let model = Private.findEntity(Connection.formatId(targetId));
+
+  let service = new ProjectService(
+    global.LUNCHBADGER_CONFIG.projectApiUrl,
+    global.LUNCHBADGER_CONFIG.workspaceApiUrl,
+    global.loginManager.user.id_token);
+
+  let modelConfig = {
+    name: model.name,
+    id: `server.${model.name}`,
+    facetName: 'server',
+    dataSource: dataSource.name,
+    public: model.public
+  };
+
+  service.upsertModelConfig(modelConfig).then(() => {
+    connectionInfo.connection.removeType('wip');
+  });
 
   dispatch('AddConnection', {
     from: connectionInfo.sourceId,
--- plugins/lunchbadger-compose/src/actions/Connection/reattach.js
+++ plugins/lunchbadger-compose/src/actions/Connection/reattach.js
@@ -1,14 +1,49 @@
-import {notify} from 'react-notify-toast';
+import Backend from '../../stores/Backend';
 
 const {dispatch} = LunchBadgerCore.dispatcher.AppDispatcher;
+const Connection = LunchBadgerCore.stores.Connection;
+const Private = LunchBadgerManage.stores.Private;
+const ProjectService = LunchBadgerCore.services.ProjectService;
 
 export default (connectionInfo) => {
   connectionInfo.connection.setType('wip');
 
-  setTimeout(() => {
+  let service = new ProjectService(
+    global.LUNCHBADGER_CONFIG.projectApiUrl,
+    global.LUNCHBADGER_CONFIG.workspaceApiUrl,
+    global.loginManager.user.id_token);
+
+
+  let {originalTargetId, newSourceId, newTargetId} = connectionInfo;
+  let promise = Promise.resolve(null);
+
+  if (originalTargetId !== newTargetId) {
+    // Moved the model end, have to remove data source from original model
+    let originalModel = Private.findEntity(Connection.formatId(originalTargetId));
+
+    promise = promise.then(() => {
+      return service.upsertModelConfig({
+        name: originalModel.name,
+        id: `server.${originalModel.name}`,
+        facetName: 'server',
+        dataSource: null,
+        public: originalModel.public
+      });
+    });
+  }
+
+  let dataSource = Backend.findEntity(Connection.formatId(newSourceId));
+  let model = Private.findEntity(Connection.formatId(newTargetId));
+
+  promise = promise.then(() => service.upsertModelConfig({
+    name: model.name,
+    id: `server.${model.name}`,
+    facetName: 'server',
+    dataSource: dataSource.name,
+    public: model.public
+  })).then(() => {
     connectionInfo.connection.removeType('wip');
-    notify.show('Model successfully reattached to data source', 'success');
-  }, 3000);
+  });
 
   dispatch('MoveConnection', {
     from: connectionInfo.originalSourceId,

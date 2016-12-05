fd5a06e20316282e4c9fa4b7bf7cc81f69dcd4d0
altsang <agilecto@gmail.com>
2016-12-05 12:49:10 -0800

    renamed ethereum, added sfdc back

---
+++ plugins/lunchbadger-compose/src/components/Tools/Salesforce.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import AddDataSource from '../../actions/CanvasElements/DataSource/add';
+
+const Tool = LunchBadgerCore.components.Tool;
+
+class Ethereum extends Component {
+  render() {
+    return (
+      <div className="salesforce tool__context__item" onClick={() => AddDataSource('Salesforce', 'salesforce')}>
+      	<i className="tool__icon icon-icon-datasource"/>
+      	<span className="tool__name">Salesforce</span>
+        <span className="tool__context__tooltip">Data Source</span>
+      </div>
+    );
+  }
+}
+
+export default Tool(Salesforce);

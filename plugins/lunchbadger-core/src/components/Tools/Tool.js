63ae4ad6cba5fe474bc9d5042d28356c1cf91262
Krzysztof Olbinski <kristof@lunchbadger.com>
2017-05-02 23:56:41 +0200

    Fix for DataSource menu item

--- plugins/lunchbadger-core/src/components/Tools/Tool.js
+++ plugins/lunchbadger-core/src/components/Tools/Tool.js
@@ -2,6 +2,17 @@ import React, {Component} from 'react';
 import classNames from 'classnames';
 import './Tool.scss';
 
+const databases = [
+  'Memory',
+  'REST',
+  'SOAP',
+  'Redis',
+  'MongoDB',
+  'MySQL',
+  'Ethereum',
+  'Salesforce'
+];
+
 export default (ComposedComponent) => {
   return class Tool extends Component {
     constructor(props) {
@@ -13,6 +24,9 @@ export default (ComposedComponent) => {
       let isToolSelected = currentEditElement === ComposedComponent.name;
       if (ComposedComponent.name === 'Endpoint') {
         isToolSelected = currentEditElement.endsWith(ComposedComponent.name);
+      }
+      if (ComposedComponent.name === 'DataSource' && databases.includes(currentEditElement)) {
+        isToolSelected = true;
       }
       return <div className={classNames({['tool--selected']: isToolSelected})}>
         <ComposedComponent {...this.props} {...this.state} />

686196817c2d45d3fd88b907182ceab9ad492a29
Krzysztof Olbinski <kristof@lunchbadger.com>
2017-05-09 18:55:10 +0200

    Contextual Notification

--- plugins/lunchbadger-ui/src/Form/Input.js
+++ plugins/lunchbadger-ui/src/Form/Input.js
@@ -33,6 +33,12 @@ class Input extends Component {
     }
   }
 
+  _handleFocus(event) {
+    if (typeof this.props.handleFocus === 'function') {
+      this.props.handleFocus(event);
+    }
+  }
+
   _handleBlur(event) {
     if (typeof this.props.handleBlur === 'function') {
       this.props.handleBlur(event);
@@ -52,6 +58,7 @@ class Input extends Component {
         className={this.props.className || ''}
         value={this.props.getValue()}
         type={this.props.type || 'text'}
+        onFocus={this._handleFocus.bind(this)}
         onBlur={this._handleBlur.bind(this)}
         onKeyPress={this._handleKeyPress.bind(this)}
         onKeyUp={this._handleKeyUp.bind(this)}

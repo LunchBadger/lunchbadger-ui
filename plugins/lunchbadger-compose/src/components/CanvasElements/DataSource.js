5cf0900465974a42bbe36a622a131581ce7b02fa
Krzysztof Olbinski <kristof@lunchbadger.com>
2017-05-11 15:42:28 +0200

    Password should be at least 6 chars long - fix

--- plugins/lunchbadger-compose/src/components/CanvasElements/DataSource.js
+++ plugins/lunchbadger-compose/src/components/CanvasElements/DataSource.js
@@ -102,7 +102,7 @@ class DataSource extends Component {
         invalid: data.password,
         onBlur: this.handleFieldChange('password'),
         password: true,
-        contextual: 'Password should be at least 3 chars long',
+        contextual: 'Password should be at least 6 chars long',
       },
     ];
     return <EntityProperties properties={mainProperties} />;

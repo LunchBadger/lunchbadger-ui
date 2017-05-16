7a65e7b846a1afe3ea6be67ad96d1b139a197682
Krzysztof Olbinski <kristof@lunchbadger.com>
2017-05-16 15:51:14 +0200

    Storybook add-on options

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import {configure, addDecorator, setAddon} from '@kadira/storybook';
 import infoAddon from '@kadira/react-storybook-addon-info';
+import {setOptions} from '@kadira/storybook-addon-options';
+
+setOptions({
+  name: 'LunchBadger',
+  url: 'https://app.lunchbadger.com',
+  goFullScreen: false,
+  showLeftPanel: true,
+  showDownPanel: true,
+  showSearchBox: false,
+  downPanelInRight: true,
+  sortStoriesByKind: false,
+});
 
 setAddon(infoAddon);
 

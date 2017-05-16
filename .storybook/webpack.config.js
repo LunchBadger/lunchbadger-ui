ac9b74785eda8d4c8a7ce5d83035aa99b28a079f
Krzysztof Olbinski <kristof@lunchbadger.com>
2017-05-16 15:04:23 +0200

    Storybook Header

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -4,20 +4,60 @@ module.exports = {
   module: {
     loaders: [
       {
-        test: /\.scss$/,
-        loader: 'style-loader!css-loader!postcss-loader!sass-loader?outputStyle=expanded',
-        include: path.resolve(__dirname, '../')
+        test: /\.css$/,
+        loader: 'style-loader!css-loader!postcss-loader'
       },
       {
+        test: /\.sass/,
+        loader: 'style-loader!css-loader!postcss-loader!sass-loader?outputStyle=expanded&indentedSyntax'
+      },
+      {
+        test: /\.scss/,
+        loader: 'style-loader!css-loader!postcss-loader!sass-loader?outputStyle=expanded'
+      },
+      {
+        test: /\.less/,
+        loader: 'style-loader!css-loader!postcss-loader!less-loader'
+      },
+      {
+        test: /\.styl/,
+        loader: 'style-loader!css-loader!postcss-loader!stylus-loader'
+      },
+      {
+        test: /\.(png|jpg|gif|woff|woff2)$/,
+        loader: 'url-loader?limit=8192'
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
+      },
+      {
+        test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url-loader?limit=10000&mimetype=application/font-woff"
+      }, {
+        test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url-loader?limit=10000&mimetype=application/font-woff"
+      }, {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url-loader?limit=10000&mimetype=application/octet-stream"
+      }, {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "file-loader"
+      }, {
         test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
         loader: "svg-inline-loader",
-        include: path.join(__dirname, '/../src/icons')
-      },
-      {
+        include: path.join(__dirname, '/../src/icons'),
+      }, {
         test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
         loader: "url-loader?limit=10000&mimetype=image/svg+xml",
         exclude: path.join(__dirname, '/../src/icons'),
-      },
+      }, {
+        test: require.resolve('jsplumb'),
+        loaders: [
+          'imports?this=>window',
+          'script'
+        ]
+      }
     ]
   }
 }

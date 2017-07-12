63dfde640132de955b1455f4052fffed123b88dc
Krzysztof Olbinski <kristof@lunchbadger.com>
2017-07-12 17:40:09 +0200

    Fix gateway/add_pipeline test

--- test/specs/gateway/add_pipeline.js
+++ test/specs/gateway/add_pipeline.js
@@ -3,38 +3,17 @@ module.exports = {
   'Gateway: add pipeline': function (browser) {
     var page = browser.page.lunchBadger();
     var elementSelector = '.quadrant:nth-child(3) .Entity.Gateway:last-child';
-
     page.open();
-
     page.addElement('gateway');
-
-    browser.pause(3500);
-
-    page.expect.element(elementSelector + ' .EntitySubElements__main').to.be.present;
-
+    browser.waitForElementVisible(elementSelector + ' .EntitySubElements__main', 60000);
     page.click(elementSelector + ' .EntitySubElements__main .EntitySubElements__title__add');
-
-    browser.pause(500);
-
-    page.expect.element(elementSelector + ' .EntitySubElements__elements .CollapsibleProperties:nth-child(2)').to.be.present;
-
+    browser.waitForElementVisible(elementSelector + ' .EntitySubElements__elements .Pipeline:nth-child(2)', 60000);
     page.click(elementSelector + ' .EntitySubElements__main .EntitySubElements__title__add');
-
-    browser.pause(500);
-
-    page.expect.element(elementSelector + ' .EntitySubElements__elements .CollapsibleProperties:nth-child(3)').to.be.present;
-
-    browser.pause(500);
-
+    browser.waitForElementVisible(elementSelector + ' .EntitySubElements__elements .Pipeline:nth-child(3)', 60000);
     page.click(elementSelector + ' .EntitySubElements__elements .CollapsibleProperties:first-child .CollapsibleProperties__bar__left');
-
-    browser.pause(1500);
-
-    page.expect.element(elementSelector + ' .EntitySubElements__elements .CollapsibleProperties:first-child .CollapsibleProperties__collapsible').to.be.present;
-
-    page.expect.element(elementSelector + ' .EntitySubElements__elements .CollapsibleProperties:first-child .CollapsibleProperties__collapsible .EntityPropertyLabel').text.to.equal('POLICIES');
-    page.assert.elementsCount(elementSelector + ' .EntitySubElements__elements .CollapsibleProperties:first-child .CollapsibleProperties__collapsible .EntityProperty', 3)
-
+    browser.waitForElementVisible(elementSelector + ' .EntitySubElements__elements .Pipeline:first-child .CollapsibleProperties__collapsible', 60000);
+    page.expect.element(elementSelector + ' .EntitySubElements__elements .Pipeline:first-child .CollapsibleProperties__collapsible .EntityPropertyLabel').text.to.equal('POLICIES');
+    page.assert.elementsCount(elementSelector + ' .EntitySubElements__elements .Pipeline:first-child .CollapsibleProperties__collapsible .EntityProperty', 3)
     page.close();
   }
 };

904b5e34be2ddef1013409b15e611a7840cba78f
Krzysztof Olbinski <kristof@lunchbadger.com>
2017-08-05 20:12:14 +0200

    refactoring

--- plugins/lunchbadger-core/src/components/Panel/PanelContainer.js
+++ plugins/lunchbadger-core/src/components/Panel/PanelContainer.js
@@ -2,8 +2,6 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import {createSelector} from 'reselect';
-import DetailsPanel from './DetailsPanel';
-import SettingsPanel from './SettingsPanel';
 import classNames from 'classnames';
 import './PanelContainer.scss';
 
@@ -14,43 +12,22 @@ class PanelContainer extends Component {
     header: PropTypes.func.isRequired,
   };
 
-  renderPanels() {
-    const {panels, canvas, header, container} = this.props;
-    return panels.map((panel, idx) => {
-      const PanelComponent = panel;
-      return (
-        <PanelComponent
-          key={idx}
-          canvas={canvas}
-          header={header}
-          container={container}
-        />
-      );
-    });
-  }
-
   render() {
-    const {disabled, canvas, plugins, header, appState, container} = this.props;
+    const {disabled, canvas, panels, header, container} = this.props;
     const panelContainerClass = classNames({
       'panel-container': true,
       'panel-container--disabled': disabled,
     });
     return (
       <div className={panelContainerClass}>
-        <DetailsPanel
-          canvas={canvas}
-          plugins={plugins}
-          header={header}
-          appState={appState}
-          container={container}
-        />
-        {this.renderPanels()}
-        <SettingsPanel
-          canvas={canvas}
-          header={header}
-          appState={appState}
-          container={container}
-        />
+        {panels.map((PanelComponent, idx) => (
+          <PanelComponent
+            key={idx}
+            canvas={canvas}
+            header={header}
+            container={container}
+          />
+        ))}
       </div>
     );
   }

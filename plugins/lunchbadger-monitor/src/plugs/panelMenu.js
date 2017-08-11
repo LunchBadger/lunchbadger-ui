14ae00b84453480a7b4a3907fbe9e31ed149046a
Krzysztof Olbinski <kristof@lunchbadger.com>
2017-08-12 00:25:05 +0200

    refactoring

--- plugins/lunchbadger-monitor/src/plugs/panelMenu.js
+++ plugins/lunchbadger-monitor/src/plugs/panelMenu.js
@@ -1,9 +1,10 @@
+import {METRICS_PANEL} from '../components/Panel/MetricsPanel';
 const {coreActions} = LunchBadgerCore.utils;
 
 export default {
   3: {
     icon: 'icon-icon-metrics',
-    panel: 'METRICS_PANEL',
-    action: coreActions.togglePanel('METRICS_PANEL'),
+    panel: METRICS_PANEL,
+    action: coreActions.togglePanel(METRICS_PANEL),
   },
 };

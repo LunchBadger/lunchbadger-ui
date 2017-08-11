56ccb2431402689dada4092f090faa905e7c43b4
Krzysztof Olbinski <kristof@lunchbadger.com>
2017-08-11 17:06:16 +0200

    refactoring

--- plugins/lunchbadger-core/src/plugs/onProjectSave.js
+++ plugins/lunchbadger-core/src/plugs/onProjectSave.js
@@ -1,7 +1,26 @@
+const statesToSave = [
+  'currentElement',
+  'currentlyOpenedPanel',
+];
+
+const setStatesToSave = (state) => {
+  const states = [];
+  statesToSave.forEach((key) => {
+    const value = state.states[key];
+    if (!state.states.hasOwnProperty(key)) return;
+    if (typeof value.toJSON === 'function') {
+      states.push({key, value: {...value.toJSON(), type: value.constructor.type}});
+    } else {
+      states.push({key, value});
+    }
+  });
+  return states;
+}
+
 export default [
   state => ({
     name: 'main',
     connections: state.connections,
-    states: [],
+    states: setStatesToSave(state),
   }),
 ];
--- plugins/lunchbadger-core/src/reducers/states.js
+++ plugins/lunchbadger-core/src/reducers/states.js
@@ -3,6 +3,11 @@ import {actionTypes} from '../reduxActions/actions';
 export default (state = {}, action) => {
   const newState = {...state};
   switch (action.type) {
+    case actionTypes.onLoadProject:
+      action.payload.body.states.forEach((item) => {
+        newState[item.key] = item.value;
+      });
+      return newState;
     case actionTypes.setState:
       newState[action.payload.key] = action.payload.value;
       return newState;

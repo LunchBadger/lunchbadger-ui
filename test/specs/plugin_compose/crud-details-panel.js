8d3f7c8f64953ae652982564bdb5341fe2f7390a
Krzysztof Olbinski <kristof@lunchbadger.com>
2017-09-05 17:46:21 +0200

    Test fix

--- test/specs/plugin_compose/crud-details-panel.js
+++ test/specs/plugin_compose/crud-details-panel.js
@@ -9,7 +9,7 @@ function getModelSelector (nth) {
 }
 
 function getDataSourceFieldSelector (nth) {
-  return getDataSourceSelector(2) + '.editable .EntityProperties .EntityProperty:nth-child(' + nth + ') .EntityProperty__field--input input';
+  return getDataSourceSelector(2) + ' .EntityProperties .EntityProperty:nth-child(' + nth + ') .EntityProperty__field--input input';
 }
 
 module.exports = {
@@ -48,15 +48,15 @@ module.exports = {
     // create Car model
     page.addElement('model');
     browser.waitForElementPresent(getModelSelector(1) + ' .submit', 5000);
-    page.clearValue(getModelSelector(1) + '.editable .EntityHeader .EntityProperty__field--input input');
+    page.clearValue(getModelSelector(1) + ' .EntityHeader .EntityProperty__field--input input');
     browser.pause(1000);
-    page.clearValue(getModelSelector(1) + '.editable .EntityHeader .EntityProperty__field--input input');
+    page.clearValue(getModelSelector(1) + ' .EntityHeader .EntityProperty__field--input input');
     browser.pause(1000);
-    page.setValueSlow(getModelSelector(1) + '.editable .EntityHeader .EntityProperty__field--input input', '');
+    page.setValueSlow(getModelSelector(1) + ' .EntityHeader .EntityProperty__field--input input', '');
     browser.pause(1000);
-    page.setValueSlow(getModelSelector(1) + '.editable .EntityHeader .EntityProperty__field--input input', '');
+    page.setValueSlow(getModelSelector(1) + ' .EntityHeader .EntityProperty__field--input input', '');
     browser.pause(1000);
-    page.setValueSlow(getModelSelector(1) + '.editable .EntityHeader .EntityProperty__field--input input', 'Car');
+    page.setValueSlow(getModelSelector(1) + ' .EntityHeader .EntityProperty__field--input input', 'Car');
     browser.pause(1000);
     browser.moveToElement(getModelSelector(1) + ' .submit', 5, 5, function() {
       browser.click(getModelSelector(1) + ' .submit');
@@ -67,15 +67,15 @@ module.exports = {
     // create Driver model
     page.addElement('model');
     browser.waitForElementPresent(getModelSelector(2) + ' .submit', 5000);
-    page.clearValue(getModelSelector(2) + '.editable .EntityHeader .EntityProperty__field--input input');
+    page.clearValue(getModelSelector(2) + ' .EntityHeader .EntityProperty__field--input input');
     browser.pause(1000);
-    page.clearValue(getModelSelector(2) + '.editable .EntityHeader .EntityProperty__field--input input');
+    page.clearValue(getModelSelector(2) + ' .EntityHeader .EntityProperty__field--input input');
     browser.pause(1000);
-    page.setValueSlow(getModelSelector(2) + '.editable .EntityHeader .EntityProperty__field--input input', '');
+    page.setValueSlow(getModelSelector(2) + ' .EntityHeader .EntityProperty__field--input input', '');
     browser.pause(1000);
-    page.setValueSlow(getModelSelector(2) + '.editable .EntityHeader .EntityProperty__field--input input', '');
+    page.setValueSlow(getModelSelector(2) + ' .EntityHeader .EntityProperty__field--input input', '');
     browser.pause(1000);
-    page.setValueSlow(getModelSelector(2) + '.editable .EntityHeader .EntityProperty__field--input input', 'Driver');
+    page.setValueSlow(getModelSelector(2) + ' .EntityHeader .EntityProperty__field--input input', 'Driver');
     browser.pause(1000);
     browser.moveToElement(getModelSelector(2) + ' .submit', 5, 5, function() {
       browser.click(getModelSelector(2) + ' .submit');

955965e6db6aafd58894ae9b9888e479be8d7f54
Krzysztof Olbinski <kristof@lunchbadger.com>
2017-09-18 19:38:22 +0200

    Fix test

--- test/specs/general/dependency-install.js
+++ test/specs/general/dependency-install.js
@@ -1,12 +1,12 @@
 var page;
 
-function expectStatus(browser, status) {
+function expectStatus(browser, status, error) {
   browser.waitForElementVisible('.workspace-status .workspace-status__progress', 120000);
   browser.waitForElementPresent(`.workspace-status .workspace-status__${status}`, 5 * 60 * 1000);
   if (status === 'failure') {
     browser.click('.workspace-status span');
     browser.waitForElementPresent('.SystemDefcon1 .SystemDefcon1__box__content__details--box', 5000);
-    page.expect.element('.SystemDefcon1 .SystemDefcon1__box__content__details--box').text.to.contain('ENOTFOUND');
+    page.expect.element('.SystemDefcon1 .SystemDefcon1__box__content__details--box').text.to.contain(error).before(5 * 60 * 1000);
   }
 }
 
@@ -15,7 +15,6 @@ module.exports = {
   'Connector installation: data source add': function(browser) {
     page = browser.page.lunchBadger();
     page.open();
-    browser.click('.workspace-status span');
     page.addElementFromTooltip('dataSource', 'rest');
     page.setValueSlow(page.getDataSourceSelector(1) + ' .input__operations0templateurl input', 'http://dumpUrl');
     page.submitCanvasEntity(page.getDataSourceSelector(1));
@@ -24,9 +23,9 @@ module.exports = {
 
   'Connector installation: add more data source': function(browser) {
     page.addElementFromTooltip('dataSource', 'soap');
-    page.setValueSlow(page.getDataSourceSelector(2) + ' .input__url input', 'http://dumpUrl');
+    page.setValueSlow(page.getDataSourceSelector(2) + ' .input__url input', 'https://www.lunchbadger.com');
     page.submitCanvasEntity(page.getDataSourceSelector(2));
-    browser.waitForElementPresent('.SystemDefcon1', 5 * 60 * 1000);
+    expectStatus(browser, 'failure', 'WSDL');
     browser.click('.SystemDefcon1 button');
     browser.waitForElementNotPresent('.SystemDefcon1', 5000);
     page.addElementFromTooltip('dataSource', 'mongodb');
@@ -36,7 +35,7 @@ module.exports = {
     page.setValueSlow(page.getDataSourceSelector(3) + ' .input__username input', 'dumpUsername');
     page.setValueSlow(page.getDataSourceSelector(3) + ' .input__password input', 'dumpPassword');
     page.submitCanvasEntity(page.getDataSourceSelector(3));
-    expectStatus(browser, 'failure');
+    expectStatus(browser, 'failure', 'ENOTFOUND');
   },
 
   'Connector uninstallation: remove datasource': function(browser) {
@@ -47,14 +46,18 @@ module.exports = {
     browser.click(page.getDataSourceSelector(3) + ' .Toolbox__button--delete');
     browser.waitForElementPresent('.ConfirmModal .confirm', 5000);
     browser.click('.ConfirmModal .confirm');
-    expectStatus(browser, 'failure');
+    expectStatus(browser, 'failure', 'WSDL');
   },
 
   'Connector uninstallation: trash workspace and reload page': function(browser) {
     browser.click('.SystemDefcon1 button');
     browser.click('.header__menu__element .fa-trash-o');
+    browser.waitForElementPresent('.spinner__overlay', 5000);
+    browser.waitForElementPresent('.workspace-status .workspace-status__progress', 120000);
+    browser.waitForElementNotPresent('.spinner__overlay', 60000);
+    browser.waitForElementNotPresent('.workspace-status .workspace-status__progress', 120000);
     browser.refresh(function () {
-      expectStatus(browser, 'success');
+      browser.waitForElementVisible('.workspace-status .workspace-status__success', 120000);
     });
   },
 

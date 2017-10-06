668fcf1a2b175415d6d12e36053a17aae9b33ab2
Krzysztof Olbinski <kristof@lunchbadger.com>
2017-10-06 22:12:53 +0200

    Datasource <-> Function connections fix

--- plugins/lunchbadger-compose/src/plugs/onConnectionCreatedStrategy.js
+++ plugins/lunchbadger-compose/src/plugs/onConnectionCreatedStrategy.js
@@ -1,16 +1,17 @@
 import {attach} from '../reduxActions/connections';
 
 const Strategy = LunchBadgerCore.models.Strategy;
-const {storeUtils} = LunchBadgerCore.utils;
+const {storeUtils: {isInQuadrant, formatId, findEntity}} = LunchBadgerCore.utils;
 const {Connections} = LunchBadgerCore.stores;
 
 const checkConnection = (info) => (_, getState) => {
   const {sourceId, targetId} = info;
   const state = getState();
-  const isBackend = storeUtils.isInQuadrant(state, 0, sourceId);
-  const isPrivate = storeUtils.isInQuadrant(state, 1, targetId);
+  if (findEntity(state, 1, targetId).constructor.type === 'Function') return null;
+  const isBackend = isInQuadrant(state, 0, sourceId);
+  const isPrivate = isInQuadrant(state, 1, targetId);
   if (isBackend && isPrivate) {
-    return !Connections.search({toId: storeUtils.formatId(targetId)}).length;
+    return !Connections.search({toId: formatId(targetId)}).length;
   }
   return null;
 };

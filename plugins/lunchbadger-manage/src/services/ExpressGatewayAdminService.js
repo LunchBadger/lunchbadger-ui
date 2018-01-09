0b88eecc7fb95e75200451ea46f4b1fdae6befbf
Krzysztof Olbinski <olbik80@gmail.com>
2018-01-09 20:36:54 +0100

    Clear code

--- plugins/lunchbadger-manage/src/services/ExpressGatewayAdminService.js
+++ plugins/lunchbadger-manage/src/services/ExpressGatewayAdminService.js
@@ -8,50 +8,23 @@ class ExpressGatewayAdminService {
 
   // getProjectId = () => `${getUser().profile.sub}-${Config.get('envId')}`;
 
-  getServiceEndpoints = () => {
-    console.log('EGADMIN CALL', 'getServiceEndpoints', `${this.api.url}/service-endpoints`);
-    return this.api.get('/service-endpoints');
-  }
+  getServiceEndpoints = () => this.api.get('/service-endpoints');
 
-  deleteServiceEndpoint = name => {
-    console.log('EGADMIN CALL', 'deleteServiceEndpoint', `${this.api.url}/service-endpoints/${name}`);
-    return this.api.delete(`/service-endpoints/${name}`);
-  }
+  deleteServiceEndpoint = name => this.api.delete(`/service-endpoints/${name}`);
 
-  putServiceEndpoint = (name, body) => {
-    console.log('EGADMIN CALL', 'putServiceEndpoint', `${this.api.url}/service-endpoints/${name}`, {body});
-    return this.api.put(`/service-endpoints/${name}`, {body});
-  }
+  putServiceEndpoint = (name, body) => this.api.put(`/service-endpoints/${name}`, {body});
 
-  getApiEndpoints = () => {
-    console.log('EGADMIN CALL', 'getApiEndpoints', `${this.api.url}/api-endpoints`);
-    return this.api.get('/api-endpoints');
-  }
+  getApiEndpoints = () => this.api.get('/api-endpoints');
 
-  deleteApiEndpoint = name => {
-    console.log('EGADMIN CALL', 'deleteApiEndpoint', `${this.api.url}/api-endpoints/${name}`);
-    return this.api.delete(`/api-endpoints/${name}`);
-  }
+  deleteApiEndpoint = name => this.api.delete(`/api-endpoints/${name}`);
 
-  putApiEndpoint = (name, body) => {
-    console.log('EGADMIN CALL', 'putApiEndpoint', `${this.api.url}/api-endpoints/${name}`, {body});
-    return this.api.put(`/api-endpoints/${name}`, {body});
-  }
+  putApiEndpoint = (name, body) => this.api.put(`/api-endpoints/${name}`, {body});
 
-  getPipelines = () => {
-    console.log('EGADMIN CALL', 'getPipelines', `${this.api.url}/pipelines`);
-    return this.api.get('/pipelines');
-  }
+  getPipelines = () => this.api.get('/pipelines');
 
-  deletePipeline = name => {
-    console.log('EGADMIN CALL', 'deletePipeline', `${this.api.url}/pipelines/${name}`);
-    return this.api.delete(`/pipelines/${name}`);
-  }
+  deletePipeline = name => this.api.delete(`/pipelines/${name}`);
 
-  putPipeline = (name, body) => {
-    console.log('EGADMIN CALL', 'putPipeline', `${this.api.url}/pipelines/${name}`, {body});
-    return this.api.put(`/pipelines/${name}`, {body});
-  }
+  putPipeline = (name, body) => this.api.put(`/pipelines/${name}`, {body});
 
 }
 

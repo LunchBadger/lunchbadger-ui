7db05ddeed88f15b7a18c6083a872959a177cd2c
Krzysztof Olbinski <olbik80@gmail.com>
2018-01-09 19:02:42 +0100

    config fix

--- src/config/dev.js
+++ src/config/dev.js
@@ -1,13 +1,19 @@
+const env = process.env.LB_ENV || 'staging'; // localhost|staging|triton
+
+const isPrefix = env !== 'localhost';
+const prefix = isPrefix ? `${env}-` : '';
+const subdomain = isPrefix ? `${env}.` : '';
+
 export default {
-  configStoreApiUrl: 'http://api.lunchbadger.com/api',
-  gitBaseUrl: 'http://api.lunchbadger.com/git',
-  projectApiUrl: 'http://internal-{USER}-{ENV}.lunchbadger.io/project-api/api',
-  workspaceApiUrl: 'http://internal-{USER}-{ENV}.lunchbadger.io/workspace-api/api',
-  forecastApiUrl: 'http://internal-{USER}-{ENV}.lunchbadger.io/project-api/api',
-  workspaceUrl: 'http://{USER}-{ENV}.lunchbadger.io',
-  expressGatewayAdminApiUrl: 'http://admin-{NAME}-{USER}-{ENV}.lunchbadger.io',
+  configStoreApiUrl: isPrefix ? `http://${prefix}api.lunchbadger.com/api` : 'http://localhost:3002/api',
+  gitBaseUrl: isPrefix ? `http://${prefix}api.lunchbadger.com/git` : 'http://localhost:3002/git',
+  projectApiUrl: isPrefix ? `http://internal-{USER}-{ENV}.${subdomain}lunchbadger.io/project-api/api` : 'http://localhost:4230/api',
+  workspaceApiUrl: isPrefix ? `http://internal-{USER}-{ENV}.${subdomain}lunchbadger.io/workspace-api/api` : 'http://localhost:4231/api',
+  forecastApiUrl: isPrefix ? `http://internal-{USER}-{ENV}.${subdomain}lunchbadger.io/project-api/api` : 'http://localhost:4230/api',
+  workspaceUrl: isPrefix ? `http://{USER}-{ENV}.${subdomain}lunchbadger.io` : 'http://localhost:3000',
+  expressGatewayAdminApiUrl: `http://admin-{NAME}-{USER}-{ENV}.${subdomain}lunchbadger.io`,
   customerUrl: 'http://workspace-{USER}-{ENV}.customer:3000',
-  kubeWatcherApiUrl: 'http://kube-watcher.lunchbadger.io',
+  kubeWatcherApiUrl: isPrefix ? `http://kube-watcher.${prefix}api.lunchbadger.com` : 'http://localhost:7788',
   user: {
     sub: 'demo',
     email: 'foo@lunchbadger.com',
@@ -21,6 +27,7 @@ export default {
     apis: true,
     portals: true,
     metrics: true,
-    forecasts: true
+    forecasts: true,
+    kubeWatcher: true
   }
 };

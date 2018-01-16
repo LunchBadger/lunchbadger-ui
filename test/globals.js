05aacc6a17f4dde2bcb49c4b7b441c1d11bc4a89
Krzysztof Olbinski <olbik80@gmail.com>
2018-01-16 15:53:14 +0100

    test clear url fix

--- test/globals.js
+++ test/globals.js
@@ -1,7 +1,7 @@
 const request = require('request');
 
-const HOST = process.env.LBSERVER_HOST || 'localhost';
-const CLEAR_URL = `http://${HOST}:4230/api/Projects/demo-dev/clear`;
+// const HOST = process.env.LBSERVER_HOST || 'localhost';
+const CLEAR_URL = 'http://internal-demo-dev.staging.lunchbadger.io/project-api/api/Projects/demo-dev/clear'; //`http://${HOST}:4230/api/Projects/demo-dev/clear`;
 
 module.exports = {
   beforeEach: function(browser, cb) {

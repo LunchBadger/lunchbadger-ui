630078003a2ecb2de5fad21657d50a5e201a1cf5
Krzysztof Olbinski <olbik80@gmail.com>
2018-02-23 19:47:30 +0100

    Fix crud-details-panel

--- test/specs/plugin_compose/crud-details-panel.js
+++ test/specs/plugin_compose/crud-details-panel.js
@@ -1,166 +1,157 @@
 var page;
+var dataSourceSelector1;
+var dataSourceSelector2;
+var modelSelector1;
+var modelSelector2;
 
 module.exports = {
-  '@disabled': true, // FIXME by card 361
-  'Compose plugin: CRUD on details panel': function (browser) {
+  // '@disabled': true,
+  'CRUD on details panel: create datasources': function (browser) {
     page = browser.page.lunchBadger();
-    page.open();
-    page.checkEntities();
-
-    // create Memory1 datasource
-    page.addElementFromTooltip('dataSource', 'memory');
-    page.setValueSlow(page.getDataSourceSelector(1) + ' .input__name input', 'Memory1');
-    page.submitCanvasEntity(page.getDataSourceSelector(1));
-    page.checkEntities('Memory1');
-
-    // reload page and check if Memory1 is present
-    page.refresh(function () {
-      page.checkEntities('Memory1');
-
-      // create Memory2 datasource
-      page.addElementFromTooltip('dataSource', 'memory');
-      page.setValueSlow(page.getDataSourceSelector(2) + ' .input__name input', 'Memory2');
-      page.submitCanvasEntity(page.getDataSourceSelector(2));
-      page.checkEntities('Memory1,Memory2');
-
-      // reload page and check if Memory1 and Memory2 are present
-      page.refresh(function () {
-        page.checkEntities('Memory1,Memory2');
-
-        // create Car model
-        page.addElement('model');
-        page.setValueSlow(page.getModelSelector(1) + ' .input__name input', 'Car');
-        page.submitCanvasEntity(page.getModelSelector(1));
-        page.checkEntities('Memory1,Memory2', 'Car');
-
-        // reload page and check if Memory1, Memory2 and Car are present
-        page.refresh(function () {
-          page.checkEntities('Memory1,Memory2', 'Car');
-
-          // create Driver model
-          page.addElement('model');
-          page.setValueSlow(page.getModelSelector(2) + ' .input__name input', 'Driver');
-          page.submitCanvasEntity(page.getModelSelector(2));
-          page.checkEntities('Memory1,Memory2', 'Car,Driver');
-
-          // reload page and check if Memory1, Memory2, Car and Driver are present
-          page.refresh(function () {
-            page.checkEntities('Memory1,Memory2', 'Car,Driver');
-
-            // connect Memory1 with Car
-            page.openEntityInDetailsPanel(page.getModelSelector(1));
-            page.selectValueSlow('.DetailsPanel', 'dataSource', 'Memory1');
-            page.submitDetailsPanel(page.getModelSelector(1));
-
-            // check, if Memory-car connection is present
-            browser.waitForElementPresent(page.getDataSourceSelector(1) + ' .port-out > .port__anchor--connected', 50000);
-            browser.waitForElementPresent(page.getModelSelector(1) + ' .port-in > .port__anchor--connected', 50000);
-
-            // reload page and check if Memory, REST, Car, Driver and Memory-Car connection are present
-            page.refresh(function () {
-              page.checkEntities('Memory1,Memory2', 'Car,Driver');
-              browser.waitForElementPresent(page.getDataSourceSelector(1) + ' .port-out > .port__anchor--connected', 50000);
-              browser.waitForElementPresent(page.getModelSelector(1) + ' .port-in > .port__anchor--connected', 50000);
-
-              // rename Memory1 into Memory1New
-              page.editEntity(page.getDataSourceSelector(1));
-              page.setValueSlow(page.getDataSourceSelector(1) + ' .input__name input', 'Memory1New');
-              page.submitCanvasEntity(page.getDataSourceSelector(1));
-              browser.expect.element(page.getDataSourceSelector(1) + ' .EntityHeader .EntityProperty__field--text').text.to.equal('Memory1New');
-
-              // rename Car into Car1
-              page.editEntity(page.getModelSelector(1));
-              page.setValueSlow(page.getModelSelector(1) + ' .input__name input', 'Car1');
-              page.submitCanvasEntity(page.getModelSelector(1));
-              browser.expect.element(page.getModelSelector(1) + ' .EntityHeader .EntityProperty__field--text').text.to.equal('Car1');
-
-              // check, if Memory1New-Car1 connection is present
-              browser.waitForElementPresent(page.getDataSourceSelector(1) + ' .port-out > .port__anchor--connected', 50000);
-              browser.waitForElementPresent(page.getModelSelector(1) + ' .port-in > .port__anchor--connected', 50000);
-
-              // reload page and check if Memory1, REST, Car1, Driver and Memory1-Car1 connection are present
-              page.refresh(function () {
-                page.checkEntities('Memory1New,Memory2', 'Car1,Driver', 'car1,driver');
-                browser.waitForElementPresent(page.getDataSourceSelector(1) + ' .port-out > .port__anchor--connected', 50000);
-                browser.waitForElementPresent(page.getModelSelector(1) + ' .port-in > .port__anchor--connected', 50000);
-
-                // reattach connection from Memory1New to Memory2
-                page.openEntityInDetailsPanel(page.getModelSelector(1));
-                page.selectValueSlow('.DetailsPanel', 'dataSource', 'Memory2');
-                page.submitDetailsPanel(page.getModelSelector(1));
-
-                // check, if Memory2-Car1 connection is present
-                browser.waitForElementNotPresent(page.getDataSourceSelector(1) + ' .port-out > .port__anchor--connected', 50000);
-                browser.waitForElementPresent(page.getDataSourceSelector(2) + ' .port-out > .port__anchor--connected', 50000);
-                browser.waitForElementPresent(page.getModelSelector(1) + ' .port-in > .port__anchor--connected', 50000);
-
-                // reload page and check if Memory1New, Memory2, Car1, Driver and Memory2-Car1 connection are present
-                page.refresh(function () {
-                  page.checkEntities('Memory1New,Memory2', 'Car1,Driver', 'car1,driver');
-                  browser.waitForElementPresent(page.getDataSourceSelector(2) + ' .port-out > .port__anchor--connected', 50000);
-                  browser.waitForElementPresent(page.getModelSelector(1) + ' .port-in > .port__anchor--connected', 50000);
-
-                  // reattach connection from Car1 to Driver
-                  page.openEntityInDetailsPanel(page.getModelSelector(1));
-                  page.selectValueSlow('.DetailsPanel', 'dataSource', 'None');
-                  page.submitDetailsPanel(page.getModelSelector(1));
-                  page.openEntityInDetailsPanel(page.getModelSelector(2));
-                  page.selectValueSlow('.DetailsPanel', 'dataSource', 'Memory2');
-                  page.submitDetailsPanel(page.getModelSelector(2));
-
-                  // check, if Memory2-Driver connection is present
-                  browser.waitForElementNotPresent(page.getModelSelector(1) + ' .port-in > .port__anchor--connected', 50000);
-                  browser.waitForElementPresent(page.getDataSourceSelector(2) + ' .port-out > .port__anchor--connected', 50000);
-                  browser.waitForElementPresent(page.getModelSelector(2) + ' .port-in > .port__anchor--connected', 50000);
-
-                  // reload page and check if Memory1New, Memory2, Car1, Driver and Memory2-Driver connection are present
-                  page.refresh(function () {
-                    page.checkEntities('Memory1New,Memory2', 'Car1,Driver', 'car1,driver');
-                    browser.waitForElementNotPresent(page.getDataSourceSelector(1) + ' .port-out > .port__anchor--connected', 50000);
-                    browser.waitForElementNotPresent(page.getModelSelector(1) + ' .port-in > .port__anchor--connected', 50000);
-                    browser.waitForElementPresent(page.getDataSourceSelector(2) + ' .port-out > .port__anchor--connected', 50000);
-                    browser.waitForElementPresent(page.getModelSelector(2) + ' .port-in > .port__anchor--connected', 50000);
-
-                    // remove Driver
-                    page.removeEntity(page.getModelSelector(2));
-
-                    // check, if Memory2-Driver connection is also not present
-                    browser.waitForElementNotPresent(page.getDataSourceSelector(2) + ' .port-out > .port__anchor--connected', 50000);
-
-                    // reload page and check if Memory1New, Memory2, Car1 are present, and Memory2 connection out is not present
-                    page.refresh(function () {
-                      page.checkEntities('Memory1New,Memory2', 'Car1', 'car1');
-                      browser.waitForElementNotPresent(page.getDataSourceSelector(2) + ' .port-out > .port__anchor--connected', 50000);
-
-                      // connect Memory2 with Car1
-                      page.openEntityInDetailsPanel(page.getModelSelector(1));
-                      page.selectValueSlow('.DetailsPanel', 'dataSource', 'Memory2');
-                      page.submitDetailsPanel(page.getModelSelector(1));
-
-                      // check, if Memory2-Car1 connection is present
-                      browser.waitForElementPresent(page.getDataSourceSelector(2) + ' .port-out > .port__anchor--connected', 50000);
-                      browser.waitForElementPresent(page.getModelSelector(1) + ' .port-in > .port__anchor--connected', 50000);
-
-                      // remove Memory2
-                      page.removeEntity(page.getDataSourceSelector(2));
-
-                      // check, is Car1 connection in is also not present
-                      browser.waitForElementNotPresent(page.getModelSelector(1) + ' .port-in > .port__anchor--connected', 50000);
-
-                      // reload page and check, if Memory1 and Car1 are present, and Car1 connection in is not present
-                      page.refresh(function () {
-                        page.checkEntities('Memory1New', 'Car1', 'car1');
-                        browser.waitForElementNotPresent(page.getModelSelector(1) + ' .port-in > .port__anchor--connected', 50000);
-                        page.close();
-                      });
-                    });
-                  });
-                });
-              });
-            });
-          });
-        });
+    dataSourceSelector1 = page.getDataSourceSelector(1);
+    dataSourceSelector2 = page.getDataSourceSelector(2);
+    modelSelector1 = page.getModelSelector(1);
+    modelSelector2 = page.getModelSelector(2);
+    page
+      .open()
+      .addElementFromTooltip('dataSource', 'memory')
+      .setCanvasEntityName(dataSourceSelector1, 'Memory1')
+      .submitCanvasEntity(dataSourceSelector1)
+      .checkEntities('Memory1')
+      .addElementFromTooltip('dataSource', 'memory')
+      .setCanvasEntityName(dataSourceSelector2, 'Memory2')
+      .submitCanvasEntity(dataSourceSelector2)
+      .reloadPage()
+      .checkEntities('Memory1,Memory2');
+  },
+  'CRUD on details panel: create models': function () {
+    page
+      .addElement('model')
+      .setCanvasEntityName(modelSelector1, 'Car')
+      .submitCanvasEntity(modelSelector1)
+      .addElement('model')
+      .setCanvasEntityName(modelSelector2, 'Driver')
+      .submitCanvasEntity(modelSelector2)
+      .reloadPage()
+      .checkEntities('Memory1,Memory2', 'Car,Driver');
+  },
+  'CRUD on details panel: connect datasource with model': function () {
+    page
+      .openEntityInDetailsPanel(modelSelector1)
+      .selectValueSlow('.DetailsPanel', 'dataSource', 'Memory1')
+      .submitDetailsPanel(modelSelector1)
+      .check({
+        connected: {
+          [dataSourceSelector1]: ['out'],
+          [modelSelector1]: ['in']
+        },
+        notConnected: {
+          [dataSourceSelector2]: ['out'],
+          [modelSelector2]: ['in']
+        }
+      })
+      .reloadPage()
+      .checkEntities('Memory1,Memory2', 'Car,Driver')
+      .check({
+        connected: {
+          [dataSourceSelector1]: ['out'],
+          [modelSelector1]: ['in']
+        },
+        notConnected: {
+          [dataSourceSelector2]: ['out'],
+          [modelSelector2]: ['in']
+        }
       });
-    });
+  },
+  'CRUD on details panel: rename datasource and model': function () {
+    page
+      .editEntity(dataSourceSelector1)
+      .setCanvasEntityName(dataSourceSelector1, 'Memory1New')
+      .submitCanvasEntity(dataSourceSelector1)
+      .editEntity(modelSelector1)
+      .setCanvasEntityName(modelSelector1, 'Car1')
+      .submitCanvasEntity(modelSelector1)
+      .check({
+        connected: {
+          [dataSourceSelector1]: ['out'],
+          [modelSelector1]: ['in']
+        },
+        notConnected: {
+          [dataSourceSelector2]: ['out'],
+          [modelSelector2]: ['in']
+        }
+      })
+      .reloadPage()
+      .checkEntities('Memory1New,Memory2', 'Car1,Driver', 'car1,driver')
+      .check({
+        connected: {
+          [dataSourceSelector1]: ['out'],
+          [modelSelector1]: ['in']
+        },
+        notConnected: {
+          [dataSourceSelector2]: ['out'],
+          [modelSelector2]: ['in']
+        }
+      });
+  },
+  'CRUD on details panel: reattach connection between datasources': function () {
+    page
+      .openEntityInDetailsPanel(modelSelector1)
+      .selectValueSlow('.DetailsPanel', 'dataSource', 'Memory2')
+      .submitDetailsPanel(modelSelector1)
+      .check({
+        connected: {
+          [dataSourceSelector2]: ['out'],
+          [modelSelector1]: ['in']
+        },
+        notConnected: {
+          [dataSourceSelector1]: ['out'],
+          [modelSelector2]: ['in']
+        }
+      })
+      .reloadPage()
+      .checkEntities('Memory1New,Memory2', 'Car1,Driver', 'car1,driver')
+      .check({
+        connected: {
+          [dataSourceSelector2]: ['out'],
+          [modelSelector1]: ['in']
+        },
+        notConnected: {
+          [dataSourceSelector1]: ['out'],
+          [modelSelector2]: ['in']
+        }
+      });
+  },
+  'CRUD on details panel: reattach connection between models': function () {
+    page
+      .openEntityInDetailsPanel(modelSelector1)
+      .selectValueSlow('.DetailsPanel', 'dataSource', 'None')
+      .submitDetailsPanel(modelSelector1)
+      .openEntityInDetailsPanel(modelSelector2)
+      .selectValueSlow('.DetailsPanel', 'dataSource', 'Memory2')
+      .submitDetailsPanel(modelSelector2)
+      .check({
+        connected: {
+          [dataSourceSelector2]: ['out'],
+          [modelSelector2]: ['in']
+        },
+        notConnected: {
+          [dataSourceSelector1]: ['out'],
+          [modelSelector1]: ['in']
+        }
+      })
+      .reloadPage()
+      .checkEntities('Memory1New,Memory2', 'Car1,Driver', 'car1,driver')
+      .check({
+        connected: {
+          [dataSourceSelector2]: ['out'],
+          [modelSelector2]: ['in']
+        },
+        notConnected: {
+          [dataSourceSelector1]: ['out'],
+          [modelSelector1]: ['in']
+        }
+      })
+      .close();
   }
 };

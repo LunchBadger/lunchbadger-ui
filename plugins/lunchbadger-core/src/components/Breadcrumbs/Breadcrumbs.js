975ca053ec0e8ab59d2119b4974a41dbf470a1af
Krzysztof Olbinski <olbik80@gmail.com>
2018-02-23 13:57:58 +0100

    Test scenario - fake login screen

--- plugins/lunchbadger-core/src/components/Breadcrumbs/Breadcrumbs.js
+++ plugins/lunchbadger-core/src/components/Breadcrumbs/Breadcrumbs.js
@@ -4,7 +4,7 @@ import './Breadcrumbs.scss';
 
 export default ({username, envId}) => (
   <div className="breadcrumbs">
-    <span className="breadcrumbs__element">{username}</span>
+    <span className="breadcrumbs__element username">{username}</span>
     <span className="breadcrumbs__element">{envId}</span>
     <span className="breadcrumbs__element">Canvas</span>
     <WorkspaceStatus />

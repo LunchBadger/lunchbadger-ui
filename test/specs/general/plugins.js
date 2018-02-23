40a2bc5eea9646456080dba8bbb4ddfa2728c1b0
Krzysztof Olbinski <olbik80@gmail.com>
2018-02-23 14:44:58 +0100

    Test scenario - lock down to compose and manage plugins

---
+++ test/specs/general/plugins.js
@@ -0,0 +1,30 @@
+var page;
+
+module.exports = {
+  // '@disabled': true,
+  'Tools menu: plugins': function (browser) {
+    page = browser.page.lunchBadger();
+    page
+      .open()
+      .check({
+        present,
+        notPresent
+      })
+      .close();
+  }
+};
+
+const present = [
+  '.Tool.dataSource',
+  '.Tool.model',
+  '.Tool.function',
+  '.Tool.microservice',
+  '.Tool.endpoint',
+  '.Tool.gateway'
+];
+const notPresent = [
+  '.Tool.api',
+  '.Tool.portal',
+  '.header__menu__link.METRICS_PANEL',
+  '.header__menu__link.FORECASTS_PANEL'
+];

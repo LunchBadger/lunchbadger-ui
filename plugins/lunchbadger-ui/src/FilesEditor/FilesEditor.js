c5fcbaa8f586f635f007cea00adcd5a0cee3fe48
Krzysztof Olbinski <olbik80@gmail.com>
2018-03-28 18:04:01 +0200

    Serverless add capability to add file to ui component

--- plugins/lunchbadger-ui/src/FilesEditor/FilesEditor.js
+++ plugins/lunchbadger-ui/src/FilesEditor/FilesEditor.js
@@ -1,8 +1,14 @@
 import React, {PureComponent} from 'react';
 import {PropTypes} from 'prop-types';
 import Tree from 'react-ui-tree';
+import slug from 'slug';
 import cx from 'classnames';
-import {CodeEditor} from '../';
+import {findDOMNode} from 'react-dom';
+import {
+  CodeEditor,
+  IconButton,
+  Input,
+} from '../';
 import './FilesEditor.scss';
 
 export default class FilesEditor extends PureComponent {
@@ -10,24 +16,32 @@ export default class FilesEditor extends PureComponent {
     lang: PropTypes.string,
     onChange: PropTypes.func,
     files: PropTypes.object,
+    onNewFile: PropTypes.func,
+  };
+
+  static defaultProps = {
+    onNewFile: () => {},
   };
 
   constructor(props) {
     super(props);
     this.state = {
-      active: Object.keys(this.props.files)[0] || null,
+      files: props.files,
+      active: Object.keys(props.files)[0] || null,
+      newFile: false,
     };
     this.codeEditorRefs = {};
   }
 
   renderNode = (node) => {
-    const active = node.module === this.state.active;
+    const file = node.module || '';
+    const active = file === this.state.active;
     return (
       <span
-        className={cx('node', {active})}
-        onClick={this.onClickNode(node.module)}
+        className={cx('node', slug(file, {lower: true}), {active})}
+        onClick={this.onClickNode(file)}
       >
-        {node.module}
+        {file}
       </span>
     );
   };
@@ -41,15 +55,50 @@ export default class FilesEditor extends PureComponent {
 
   handleResize = size => this.setState({size});
 
+  handleAddFile = () => {
+    this.setState({newFile: true}, () => {
+      const input = findDOMNode(this.newFileRef).querySelector('input');
+      input && input.focus();
+    });
+  };
+
+  handleNewFileKeyPress = (event) => {
+    if (event.keyCode === 13 || event.which === 13) {
+      const filename = event.target.value.trim();
+      if (filename.length > 0) {
+        const files = {};
+        const data = {
+          ...this.state.files,
+          [filename]: '',
+        };
+        Object.keys(data).sort().forEach(key => files[key] = data[key]);
+        this.setState({
+          files,
+          newFile: false,
+        });
+        setTimeout(() => {
+          this.setState({active: filename}, () => {
+            this.dispatchResizeEvent();
+            const input = findDOMNode(this.filesRef).querySelector(`.${slug(filename, {lower: true})}`);
+            input && input.scrollIntoViewIfNeeded();
+          });
+        });
+      }
+      event.preventDefault();
+      event.stopPropagation();
+    }
+  };
+
   render () {
-    const {lang, onChange, files} = this.props;
+    const {lang, onChange} = this.props;
+    const {files} = this.state;
     const tree = {
       children: Object.keys(files).map(file => ({
         module: file,
         leaf: true,
       }))
     };
-    const {active, size} = this.state;
+    const {active, size, newFile} = this.state;
     return (
       <div className="FilesEditor">
         <div className="FilesEditor__codeEditor">
@@ -69,7 +118,25 @@ export default class FilesEditor extends PureComponent {
           ))}
         </div>
         <div className="FilesEditor__tree">
+          {!newFile && (
+            <div className="FilesEditor__addFile">
+              <IconButton icon="iconPlus" onClick={this.handleAddFile} />
+            </div>
+          )}
+          {newFile && (
+            <div className="FilesEditor__newFile">
+              <Input
+                ref={r => this.newFileRef = r}
+                name="tmp[newFile]"
+                fullWidth
+                value=""
+                handleBlur={() => this.setState({newFile: false})}
+                handleKeyPress={this.handleNewFileKeyPress}
+              />
+            </div>
+          )}
           <Tree
+            ref={r => this.filesRef = r}
             tree={tree}
             renderNode={this.renderNode}
             paddingLeft={0}

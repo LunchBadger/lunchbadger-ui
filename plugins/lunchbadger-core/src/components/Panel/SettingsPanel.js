7e928d6e20f2e53c17bea39a201e0527f837d6e1
Vincenzo Chianese <vincenz.chianese@icloud.com>
2018-06-15 11:05:56 +0200

    Remove reinstall, restart and reinit workspace

--- plugins/lunchbadger-core/src/components/Panel/SettingsPanel.js
+++ plugins/lunchbadger-core/src/components/Panel/SettingsPanel.js
@@ -1,13 +1,13 @@
 /* eslint-disable no-console */
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import {CopyToClipboard} from 'react-copy-to-clipboard';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 import Panel from './Panel';
 import panelKeys from '../../constants/panelKeys';
 import ProjectService from '../../services/ProjectService';
 import Config from '../../../../../src/config';
 import SshManager from './EntitiesDetails/SshManager';
-import {EntityPropertyLabel} from '../../../../lunchbadger-ui/src';
+import { EntityPropertyLabel } from '../../../../lunchbadger-ui/src';
 import './SettingsPanel.scss';
 
 class SettingsPanel extends Component {
@@ -75,36 +75,6 @@ class SettingsPanel extends Component {
               <CopyToClipboard text={serverlessGitCloneCommand}>
                 <i className="fa fa-copy iconCopy" />
               </CopyToClipboard>
-            </div>
-          </div>
-        </div>
-        <div className="details-panel__element">
-          <div className="details-panel__fieldset">
-            <label className="details-panel__label">
-              Restart application
-            </label>
-            <div className="details-panel__static-field">
-              <button onClick={this.onRestartWorkspace}>Restart</button>
-            </div>
-          </div>
-        </div>
-        <div className="details-panel__element">
-          <div className="details-panel__fieldset">
-            <label className="details-panel__label">
-              Reset application
-            </label>
-            <div className="details-panel__static-field">
-              <button onClick={this.onResetWorkspace}>Reset</button>
-            </div>
-          </div>
-        </div>
-        <div className="details-panel__element">
-          <div className="details-panel__fieldset">
-            <label className="details-panel__label">
-              Reinstall dependencies
-            </label>
-            <div className="details-panel__static-field">
-              <button onClick={this.onReinstall}>Reinstall</button>
             </div>
           </div>
         </div>

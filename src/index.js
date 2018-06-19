92fc099b11ca890268908db20146e0836d9fd9b8
Krzysztof Olbinski <olbik80@gmail.com>
2018-06-19 18:00:30 +0200

    ver

--- src/index.js
+++ src/index.js
@@ -15,13 +15,14 @@ import 'jsplumb';
 import './fonts/trench100free.css';
 import './fonts/lunchbadger.css';
 
-console.info('LBAPP VERSION 0.85', [
+console.info('LBAPP VERSION 0.86', [
   [
     'master Bugfix/675 Unauthorized error should logout #677',
     'Feature/676 settings tooltip fix #678',
     'Bugfix/673 Unaligned warnings in code editor #679',
     'Bugfix/62 canvas weird state fix #681',
-    'Feature/650 onbeforeunload #682'
+    'Feature/650 onbeforeunload #682',
+    'Bugfix/558 Functions are removed on project clear #683'
   ],
   [
     'master Feature/663 Add tooltips to header menu #671',

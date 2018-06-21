c2a7a66abb3c27b28ad19945a1fea93db68bccaa
kolbinski <olbik80@gmail.com>
2018-06-21 23:04:18 +0200

    Autosave for function connections (#701)

--- plugins/lunchbadger-compose/src/plugs/onConnectionCreatedStrategy.js
+++ plugins/lunchbadger-compose/src/plugs/onConnectionCreatedStrategy.js
@@ -1,10 +1,13 @@
-import {attach} from '../reduxActions/connections';
+import {
+  attachWithModel,
+  attachWithFunction,
+} from '../reduxActions/connections';
 
 const Strategy = LunchBadgerCore.models.Strategy;
 const {storeUtils: {isInQuadrant, formatId, findEntity}} = LunchBadgerCore.utils;
 const {Connections} = LunchBadgerCore.stores;
 
-const checkConnection = (info) => (_, getState) => {
+const checkConnectionWithModel = (info) => (_, getState) => {
   const {sourceId, targetId} = info;
   const state = getState();
   if (findEntity(state, 1, targetId).constructor.type === 'Function_') return null;
@@ -19,6 +22,26 @@ const checkConnection = (info) => (_, getState) => {
   return null;
 };
 
+const checkConnectionWithFunction = (info) => (_, getState) => {
+  const {sourceId, targetId} = info;
+  const state = getState();
+  const isSourceBackend = isInQuadrant(state, 0, sourceId);
+  const isSourcePrivate = isInQuadrant(state, 1, sourceId);
+  const isTargetPrivate = isInQuadrant(state, 1, targetId);
+  const sourceType = isSourcePrivate && findEntity(state, 1, sourceId).constructor.type;
+  const targetType = findEntity(state, 1, targetId).constructor.type;
+  const isSourceFunction = isSourcePrivate && sourceType === 'Function_';
+  if (!isSourceFunction && targetType === 'Model') return null;
+  if ((isSourceBackend || isSourceFunction) && isTargetPrivate) {
+    const conns = Connections
+      .search({toId: formatId(targetId)})
+      .filter(({info: {source}}) => source.parentElement.classList.contains('port-Function_'));
+    return !conns.length;
+  }
+  return null;
+};
+
 export default [
-  new Strategy(checkConnection, attach),
+  new Strategy(checkConnectionWithModel, attachWithModel),
+  new Strategy(checkConnectionWithFunction, attachWithFunction),
 ];

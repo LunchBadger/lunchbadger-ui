bc8d77d34934f02cbce3045d13d377fa86ab6a20
Krzysztof Olbinski <olbik80@gmail.com>
2018-06-21 21:54:56 +0200

    ver

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ import 'jsplumb';
 import './fonts/trench100free.css';
 import './fonts/lunchbadger.css';
 
-console.info('LBAPP VERSION 0.95', [
+console.info('LBAPP VERSION 0.96', [
   [
     'master Bugfix/675 Unauthorized error should logout #677',
     'Feature/676 settings tooltip fix #678',
@@ -31,7 +31,8 @@ console.info('LBAPP VERSION 0.95', [
     'Bugfix/694 Support link in walkthrough #697',
     'Feature/693 Walkthrough code commands #698',
     'Feature/612 Independent quadrant scrolls #699',
-    'Feature/695 Walkthrough closed settings #700'
+    'Feature/695 Walkthrough closed settings #700',
+    'Bugfix/688 Autosave for function connections #701'
   ],
   [
     'master Feature/663 Add tooltips to header menu #671',

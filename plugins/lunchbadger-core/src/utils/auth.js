2025dec8407544925318733723beab2259ccac43
Krzysztof Olbinski <olbik80@gmail.com>
2018-06-25 21:52:18 +0200

    kw shouldn't be called during logout

--- plugins/lunchbadger-core/src/utils/auth.js
+++ plugins/lunchbadger-core/src/utils/auth.js
@@ -45,6 +45,7 @@ export class LoginManager {
         this.user = user;
         Config.apiUrlsReplacements(this.user.profile.sub);
         window.localStorage.setItem('login_refresh_attempts', 0);
+        window.localStorage.setItem('logoutCalled', false);
         return true;
       })
       .catch(err => {
@@ -68,6 +69,7 @@ export class LoginManager {
   }
 
   logout() {
+    window.localStorage.setItem('logoutCalled', true);
     this.um.removeUser().then(() => {
       this.um.signoutRedirect();
     });
@@ -91,6 +93,7 @@ export class DummyLoginManager {
     this.user.profile.sub = fakeLogin;
     this.user.profile.preferred_username = localStorage.getItem('preferred_username');
     Config.apiUrlsReplacements(this.user.profile.sub);
+    window.localStorage.setItem('logoutCalled', false);
     return Promise.resolve(this.user);
   }
 
@@ -100,6 +103,7 @@ export class DummyLoginManager {
 
   logout() {
     if (!isStorageSupported) return;
+    window.localStorage.setItem('logoutCalled', true);
     localStorage.removeItem('fakeLogin');
     localStorage.removeItem('preferred_username');
     document.location.reload();

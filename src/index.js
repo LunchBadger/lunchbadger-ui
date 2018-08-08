087f24cf36d38f48661480019838a4a161a187d0
Krzysztof Olbinski <olbik80@gmail.com>
2018-08-08 21:34:34 +0200

    ver

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ import 'jsplumb';
 import './fonts/trench100free.css';
 import './fonts/lunchbadger.css';
 
-console.info('LBAPP VERSION 0.158', [
+console.info('LBAPP VERSION 0.159', [
   [
     'master Feature/722 admin api calls parallel with project api calls #765',
     'Feature/599 autosave apis #768',
@@ -43,7 +43,8 @@ console.info('LBAPP VERSION 0.158', [
     'Feature/809 Utilize new pod id for UI entity state comparision for functions #813',
     'Feature/810 delete gateway and function entities right away but allow name to be reused immediately #814',
     'Feature/781 Add runtime in collapsed functions #815',
-    'Feature/817 Sync UI with EG schemas #818'
+    'Feature/817 Sync UI with EG schemas #818',
+    'Feature/822 add entity id to zoom window #824'
   ],
   [
     'master Feature/399 fn types per environment #763',
@@ -58,21 +59,6 @@ console.info('LBAPP VERSION 0.158', [
     'Feature/744 add logs to function #756',
     'Bugfix/754 Connections ports alignment #759',
     'Bugfix/761 cannot save model #762'
-  ],
-  [
-    'master Bugfix/711 Python function with error cannot be edited #714',
-    'Feature/715 Error handling #716',
-    'Bugfix/598 audit autosave gateways #718',
-    'Bugfix/654 Cannot delete model from zoom window #725',
-    'Bugfix/658 Its possible to connect with deploying or crashed gateway #726',
-    'Bugfix/657 Wrong row deleted in service and api endpoint #727',
-    'Feature/640 preserve state of walkthrough after page reload #724',
-    'Feature/731 walkthrough continue after connecting with pipeline #732',
-    'Bugfix/733 ApiEndpoint is not auto-added, when connecting again service endpoint #738',
-    'Feature/728 Add python 3.6 selector for function #739',
-    'Feature/740 connection remove icon #741',
-    'Feature/721 Add validations to service endpoint urls #742',
-    'Feature/655 service endpoint first path cannot be removed #743'
   ]
 ]);
 

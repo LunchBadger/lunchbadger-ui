b3a300f13b76d6ad1fe278a8ed8e38b8d46dd98f
Krzysztof Olbinski <olbik80@gmail.com>
2018-08-09 16:03:39 +0200

    ver

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ import 'jsplumb';
 import './fonts/trench100free.css';
 import './fonts/lunchbadger.css';
 
-console.info('LBAPP VERSION 0.160', [
+console.info('LBAPP VERSION 0.161', [
   [
     'master Feature/722 admin api calls parallel with project api calls #765',
     'Feature/599 autosave apis #768',

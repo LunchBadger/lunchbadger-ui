1efcd85c78956bbadac4dab41a39c7bd3e1bdf5d
Krzysztof Olbinski <olbik80@gmail.com>
2018-08-27 18:00:41 +0200

    Removed configstore and lunchbadger-workspace from docker-compose.yml

--- test/specs/datasource/redis.js
+++ test/specs/datasource/redis.js
@@ -2,7 +2,7 @@ var page;
 var entitySelector;
 
 module.exports = {
-  // '@disabled': true,
+  '@disabled': true,
   'Redis': function (browser) {
     page = browser.page.lunchBadger();
     entitySelector = page.getDataSourceSelector(1);

e45a00ac041a673942ea8f22ff91f447bfb0b797
Krzysztof Olbinski <olbik80@gmail.com>
2018-09-03 17:02:35 +0200

    ver

--- src/index.js
+++ src/index.js
@@ -14,14 +14,15 @@ import 'jsplumb';
 import './fonts/trench100free.css';
 import './fonts/lunchbadger.css';
 
-console.info('LBAPP VERSION 0.194', [
+console.info('LBAPP VERSION 0.195', [
   [
     'master Feature/846 real time field change #867',
     'Feature/869 On function deploy failure constrain error within entity #871',
     'Feature/823 Service endpoints in EG only when connected #873',
     'Feature/884 extend model service endpoint url #885',
     'Bugfix/888 Code editor on models cannot resize horizontally, only vertically #889',
-    'Bugfix/890 sls PUT sends undefined instead of name #891'
+    'Bugfix/890 sls PUT sends undefined instead of name #891',
+    'Feature/872 app in disabled state when workspace not running #892'
   ],
   [
     'master Bugfix/835 need to suppress messages for non visible entities #839',

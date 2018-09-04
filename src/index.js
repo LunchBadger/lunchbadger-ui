35a31b884071bb3dcf1c1b7778dac6b175462f53
Krzysztof Olbinski <olbik80@gmail.com>
2018-09-04 20:22:25 +0200

    ver

--- src/index.js
+++ src/index.js
@@ -14,7 +14,8 @@ import 'jsplumb';
 import './fonts/trench100free.css';
 import './fonts/lunchbadger.css';
 
-console.info('LBAPP VERSION 0.102', [
+console.info('LBAPP VERSION 0.103', [
+  '899',
   '885 891',
   '885',
   '871',

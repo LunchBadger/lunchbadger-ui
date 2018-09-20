89ea9aa01e0155d328ba7feeffb0a5ce15659bba
Krzysztof Olbinski <olbik80@gmail.com>
2018-09-20 16:57:13 +0200

    ver

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ import 'jsplumb';
 import './fonts/trench100free.css';
 import './fonts/lunchbadger.css';
 
-console.info('LBAPP VERSION 0.231', [
+console.info('LBAPP VERSION 0.232', [
   [
     'master Feature/846 real time field change #867',
     'Feature/869 On function deploy failure constrain error within entity #871',
@@ -45,7 +45,8 @@ console.info('LBAPP VERSION 0.231', [
     'Feature/907 Model connector renaming in walkthrough #950',
     'Feature/904 walkthrough: quick edit vs full edit #951',
     'Feature/940 IE warning #952',
-    'Feature/887 re-enable manta connector on staging #953'
+    'Feature/887 re-enable manta connector on staging #953',
+    'Feature/853 Visualize empty object in policy parameters #954'
   ],
   [
     'master Bugfix/835 need to suppress messages for non visible entities #839',

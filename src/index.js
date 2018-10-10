53e6f61a90932f403b80eb9657a947361488d22a
Krzysztof Olbinski <olbik80@gmail.com>
2018-10-10 22:19:46 +0200

    ver

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import 'font-awesome/css/font-awesome.css';
 import 'jsplumb';
 import './fonts/lunchbadger.css';
 
-console.info('LBAPP VERSION 0.276', [
+console.info('LBAPP VERSION 0.277', [
   'Feature/980 ContextPath logic as in loopback #1016',
   'Feature/909 Function to model connection as dashed line #1015',
   'Feature/921 JWT secretOrPublicKey should be textarea #1014',

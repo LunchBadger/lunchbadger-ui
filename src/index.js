cfd2d6fb2bcf4995a261a88740266378627288c2
Krzysztof Olbinski <olbik80@gmail.com>
2018-10-12 13:01:12 +0200

    ver

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import 'font-awesome/css/font-awesome.css';
 import 'jsplumb';
 import './fonts/lunchbadger.css';
 
-console.info('LBAPP VERSION 0.280', [
+console.info('LBAPP VERSION 0.281', [
   'Feature/983 ApiEndpoint should inherit context path part 2 #1017',
   'Feature/980 ContextPath logic as in loopback #1016',
   'Feature/909 Function to model connection as dashed line #1015',

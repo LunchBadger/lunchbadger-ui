fab5a7b9a2b868005968c7d7f5fd377003bef1b5
Krzysztof Olbinski <olbik80@gmail.com>
2018-10-26 14:31:12 +0200

    Show validation message for model property

---
+++ plugins/lunchbadger-compose/src/components/Panel/EntitiesDetails/ModelDefaultValue.js
@@ -0,0 +1,71 @@
+import React, {PureComponent} from 'react';
+import {Input} from '../../../../../lunchbadger-ui/src';
+import './ModelDetails.scss';
+
+export default class ModelDefaultValue extends PureComponent {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      invalid: false,
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.type !== nextProps.type) {
+      this.setState({invalid: false});
+    }
+  }
+
+  handleBlur = ({target: {value}}) => {
+    const {type} = this.props;
+    if (['object', 'array', 'geopoint'].includes(type)) {
+      let invalid = false;
+      try {
+        const parsed = JSON.parse(value);
+        if (typeof parsed === 'string') {
+          invalid = true;
+        }
+        if (!invalid && type === 'geopoint' && !(
+          Array.isArray(parsed)
+          &&
+          parsed.length === 2
+          &&
+          Number.isFinite(parsed[0])
+          &&
+          Number.isFinite(parsed[1])
+        )) {
+          invalid = true;
+        }
+      } catch (e) {
+        invalid = true;
+      }
+      this.setState({invalid});
+    }
+  };
+
+  render() {
+    const {name, value, type, inputType, textarea} = this.props;
+    const {invalid} = this.state;
+    return (
+      <div>
+        <Input
+          name={name}
+          value={value}
+          underlineStyle={{bottom: 0}}
+          fullWidth
+          hideUnderline
+          type={inputType}
+          textarea={textarea}
+          handleFocus={() => this.setState({invalid: false})}
+          handleBlur={this.handleBlur}
+        />
+        {invalid && (
+          <div className="EntityProperty__error">
+            This is not a valid {type}.
+          </div>
+        )}
+      </div>
+    )
+  }
+}

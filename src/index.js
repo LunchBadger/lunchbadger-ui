7dff4bab045e53f2b4ff02ac69107a49df661919
Krzysztof Olbinski <olbik80@gmail.com>
2018-11-05 15:21:29 +0100

    ver

--- src/index.js
+++ src/index.js
@@ -13,7 +13,8 @@ import 'font-awesome/css/font-awesome.css';
 import 'jsplumb';
 import './fonts/lunchbadger.css';
 
-console.info('LBAPP VERSION 0.149', [
+console.info('LBAPP VERSION 0.150', [
+  '1058',
   '1052',
   '1046+1041',
   '1046',

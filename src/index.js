782ad957faed5229f71b28a16c3fc63f1e9724e8
Krzysztof Olbinski <kristof@lunchbadger.com>
2018-11-20 15:07:45 +0100

    ver

--- src/index.js
+++ src/index.js
@@ -13,7 +13,8 @@ import 'font-awesome/css/font-awesome.css';
 import 'jsplumb';
 import './fonts/lunchbadger.css';
 
-console.info('LBAPP VERSION 0.157', [
+console.info('LBAPP VERSION 0.158', [
+  '1085',
   '1076+1078+1079',
   '1074+1071',
   '1074',
@@ -30,9 +31,7 @@ console.info('LBAPP VERSION 0.157', [
   '1038',
   '1037',
   '1034',
-  '1031+1021+1022',
-  '1031',
-  '1017'
+  '1031+1021+1022'
 ]);
 
 // Needed for onTouchTap

c387139f2d1c4682f3628d8aca60ca6fcc4daf3d
Krzysztof Olbinski <kristof@lunchbadger.com>
2018-12-27 21:44:13 +0100

    ver

--- src/index.js
+++ src/index.js
@@ -12,7 +12,8 @@ import 'font-awesome/css/font-awesome.css';
 import 'jsplumb';
 import './fonts/lunchbadger.css';
 
-console.info('LBAPP VERSION 0.362', [
+console.info('LBAPP VERSION 0.363', [
+  'Feature/429 Inline code editor improvements #1138',
   'Feature/429 Inline code editor improvements #1138',
   'Feature/1114 Buttons in consumer management #1137',
   'Feature/352 js files size #1136',
@@ -30,8 +31,7 @@ console.info('LBAPP VERSION 0.362', [
   'Feature/1098 Triton branding back on al3 #1101',
   'Bugfix/1091 Model and ds are locking after quick edit #1100',
   'Bugfix/1093 While deploying function you can attach to gateway #1097',
-  'Use gateway name from pod name in admin api calls #1096',
-  'adding local version of sls api on port 4444 #1088'
+  'Use gateway name from pod name in admin api calls #1096'
 ]);
 
 // Google Analityca

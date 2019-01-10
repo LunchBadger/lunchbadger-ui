ec551ed1248fab29e62d51069addc48308df67b1
Krzysztof Olbinski <kristof@lunchbadger.com>
2019-01-10 16:45:39 +0100

    ver

--- src/index.js
+++ src/index.js
@@ -12,7 +12,8 @@ import 'font-awesome/css/font-awesome.css';
 import 'jsplumb';
 import './fonts/lunchbadger.css';
 
-console.info('LBAPP VERSION 0.184', [
+console.info('LBAPP VERSION 0.185', [
+  '1144',
   '1140 292513a',
   '1135+1128',
   '1135',
@@ -30,8 +31,7 @@ console.info('LBAPP VERSION 0.184', [
   '1097',
   '1084',
   '1086+1077',
-  '1085',
-  '1076+1078+1079'
+  '1085'
 ]);
 
 // Google Analityca

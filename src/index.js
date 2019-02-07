b91f9e50c4b2cf00109604fbedb6e188532cb200
Krzysztof Olbinski <kristof@lunchbadger.com>
2019-02-07 17:32:39 +0100

    tmp ver

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import 'font-awesome/css/font-awesome.css';
 import 'jsplumb';
 import './fonts/lunchbadger.css';
 
-console.info('LBAPP VERSION 0.202', [
+console.info('LBAPP VERSION 0.203', [
   '1157',
   '1153',
   '1094',
